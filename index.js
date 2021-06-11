8435f468de2b03cc2dc67f5cdef8d9475044e65a
Jonathan Sells <jsells@xcira.com>
2021-06-11 19:33:11 -0400

    adding code to initialize and open serial port of raspberry pi

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// index.js
+/**
+ * Raspberry Pi Zero W has PL011 UART assigned to Bluetooth instead of GPIO default,
+ * follow these instructions for setup of UART port https://www.programmersought.com/article/93804026224/
+ */
+const SerialPort = require('serialport');
+const port = new SerialPort('/dev/ttyAMA0', {
+    baudRate: 19200
+})
