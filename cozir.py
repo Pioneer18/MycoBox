344c8f30f08e1357fd5f6802b0adc345ee9c733f
Jonathan Sells <jsells@xcira.com>
2021-06-12 09:41:36 -0400

    flushInput() was deprecated, replaced with reset_input_buffer

--- cozir.py
+++ cozir.py
@@ -10,7 +10,7 @@ ser.write("M 4\r\n".encode()) # set display mode to show only CO2
 ser.write("K 2\r\n".encode()) # set operating mode
 # K sets the mode, 2 sets streaming instantaneous CO2 output
 # \r\n is CR and LF
-ser.flushInput()
+ser.reset_input_buffer()
 time.sleep(1)
 while True:
  ser.write("Z\r\n".encode())
