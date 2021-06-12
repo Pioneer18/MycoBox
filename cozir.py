0fbf34d29faeeebefb45ef65a53dfcf95c7b0205
Jonathan Sells <jsells@xcira.com>
2021-06-12 09:27:27 -0400

    encoding unicode strings to bytes when writing to serial port

--- cozir.py
+++ cozir.py
@@ -6,14 +6,14 @@ multiplier = 10 # 20% sensors requires a multiplier
 # follow these instructions for setup of UART port https://www.programmersought.com/article/93804026224/
 ser = serial.Serial("/dev/ttyAMA0")
 print ("Python progam to run a Cozir Sensor\n")
-ser.write("M 4\r\n") # set display mode to show only CO2
-ser.write("K 2\r\n") # set operating mode
+ser.write("M 4\r\n".encode()) # set display mode to show only CO2
+ser.write("K 2\r\n".encode()) # set operating mode
 # K sets the mode, 2 sets streaming instantaneous CO2 output
 # \r\n is CR and LF
 ser.flushInput()
 time.sleep(1)
 while True:
- ser.write("Z\r\n")
+ ser.write("Z\r\n".encode())
  resp = ser.read(10)
  resp = resp[:8]
  fltCo2 = float(resp[2:])
