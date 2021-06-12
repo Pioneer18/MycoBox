1279db3502a2284080cededb1e82e3d9d5f02fe5
Jonathan Sells <jsells@xcira.com>
2021-06-12 10:10:48 -0400

    adding some logs

--- cozir.py
+++ cozir.py
@@ -10,7 +10,9 @@ ser.write("M 4\r\n".encode()) # set display mode to show only CO2
 ser.write("K 2\r\n".encode()) # set operating mode
 # K sets the mode, 2 sets streaming instantaneous CO2 output
 # \r\n is CR and LF
+print('wrote to the sensor')
 ser.reset_input_buffer()
+print('reset the input buffer')
 time.sleep(1)
 while True:
  ser.write("Z\r\n".encode())
