be15d000d742f9d83a4ccd3d76030def5e1fe714
Jonathan Sells <jsells@xcira.com>
2021-06-14 12:02:14 -0400

    removed multiplier from cozier.py

--- cozir.py
+++ cozir.py
@@ -1,7 +1,6 @@
 #Python progam to run a COZIR CO2 Sensor
 import serial
 import time
-multiplier = 10 # 20% sensors requires a multiplier
 # Raspberry Pi Zero W has PL011 UART assigned to Bluetooth instead of GPIO default,
 # follow these instructions for setup of UART port https://www.programmersought.com/article/93804026224/
 ser = serial.Serial("/dev/ttyAMA0")
@@ -24,5 +23,5 @@ while True:
  resp = resp[:8]
  print('8 bits or response: ', resp)
  fltCo2 = float(resp[2:])
- print ("CO2 PPM = ", fltCo2 * multiplier)
+ print ("CO2 PPM = ", fltCo2)
  time.sleep(1) 
