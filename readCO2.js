9fe6e27d1e3d9ee086ebb4267b6cb209ce62b613
Jonathan Sells <jsells@xcira.com>
2021-06-15 13:39:45 -0400

    updates

--- readCO2.js
+++ readCO2.js
@@ -1,10 +1,7 @@
 const SerialPort = require('serialport');
 SerialPort.Binding = require('@serialport/bindings');
-const Readline = SerialPort.parsers.Readline;
 const serialport = new SerialPort('/dev/ttyAMA0');
-const parser = new Readline();
-serialport.pipe(parser);
-parser.on('data', console.log)
+
 console.log("Set Display Mode and Operation Mode");
 serialport.write("M 4\r\n", function(err) {
     if (err) {
@@ -17,11 +14,25 @@ serialport.write("K 2\r\n", function(err) {
     }
 });
 setTimeout(()=> {}, 1000); // pause 1 second
+//Request the CO2
+console.log('Requet the CO2');
 serialport.write("Z\r\n", function(err) {
     if (err) {
       return console.log('Error on write: ', err.message)
     }
 });
-console.log("Rquest CO2");
-resp = serialport.read(10);
-console.log('Response: ',resp);
+// Read the Port
+console.log("Read Serial Port");
+serialport.on('readable', function() {
+    console.log('Data: ', serialport.read())
+});
+// Log the port data
+serialport.on('data', function(data) {
+    console.log('Data:', data)
+});
+// Log Errors
+serialport.on('error', function(err) {
+    console.log('Error:', err.message)
+});
+const lineStream = port.pipe(new Readline());
+console.log(lineStream);
