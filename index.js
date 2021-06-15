bea99ef85dad00702143e241fc473e00729a62ac
Jonathan Sells <jsells@xcira.com>
2021-06-15 15:27:47 -0400

    added script for dht22 sensor

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { PythonShell } = require("python-shell");
 
 
-PythonShell.run('python/readCO2.py', null, function (err, results) {
+PythonShell.run('python/CO2.py', null, function (err, results) {
   if (err) throw err;
   console.log('Results: %j', results);
 });
