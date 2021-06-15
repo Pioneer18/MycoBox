2dbeded48b2abd0ecca1222501560e92725700f0
Jonathan Sells <jsells@xcira.com>
2021-06-15 15:48:55 -0400

    updated the read dht22 code

--- python/tempAndHumidity.py
+++ python/tempAndHumidity.py
@@ -1,9 +1,12 @@
 import Adafruit_DHT
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 10
-while humidity is None and temperature is None:
+success = False
+count = 0
+while success is False and count < 10:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
     if humidity is not None and temperature is not None:
         print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+        success = True
     else:
         print("Failed to retrieve data from humidity sensor")
