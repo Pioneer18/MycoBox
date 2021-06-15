c8497bc73306fe3ef76a44dfdd33974ae9cfe875
Jonathan Sells <jsells@xcira.com>
2021-06-15 15:06:11 -0400

    added a warmup time

--- python/readCO2.py
+++ python/readCO2.py
@@ -7,7 +7,7 @@ ser = serial.Serial("/dev/ttyAMA0")
 ser.write("M 4\r\n".encode()) # set display mode to show only C02
 ser.write("K 2\r\n".encode()) # set operating mode to polling
 ser.reset_input_buffer()
-time.sleep(1)
+time.sleep(3) # warmup time
 ser.write("Z\r\n".encode())
 ser.reset_input_buffer()
 resp = ser.read(10)
