279c952067f02e7191e956af83501148099603d7
Jonathan Sells <jsells@xcira.com>
2021-06-16 13:43:51 -0400

    routing

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 // just place the name of the view you want to render
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname,'index.hmtl'));
+  res.send('../views/index.html');
 });
 
 module.exports = router;
