9288e0de9a424c3d414fee0f51f687710fe9931e
Jonathan Sells <jsells@xcira.com>
2021-06-16 06:02:55 -0400

    placed index.js in new src directory

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const { PythonShell } = require("python-shell");
+
+
+PythonShell.run('../python/CO2.py', null, function (err, results) {
+  if (err) throw err;
+  console.log('Results: %j', results);
+});
+
+PythonShell.run('../python/tempAndHumidity.py', null, function (err, results) {
+  if (err) throw err;
+  console.log('Results: %j', results);
+});
