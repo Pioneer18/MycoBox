1145062232fe31577b7a98481bc58d7c21dea41d
Jonathan Sells <jsells@xcira.com>
2021-06-16 13:33:38 -0400

    usinghtml no jade

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
+const { path } = require('../app');
 var router = express.Router();
 
 /* GET home page. */
 // just place the name of the view you want to render
 router.get('/', function(req, res, next) {
-  res.render('main_configuration', { title: 'Express' });
+  res.sendFile(path.join(__dirname, '/views/index.js'));
 });
 
 module.exports = router;
