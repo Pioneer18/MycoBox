439bb77bf8b575a2a998678c2f23e97034a56d31
Jonathan Sells <jsells@xcira.com>
2021-06-16 12:11:41 -0400

    added a sample jade template form

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// just place the name of the view you want to render
 router.get('/', function(req, res, next) {
-  res.render('error', { title: 'Express' });
+  res.render('main_configuration', { title: 'Express' });
 });
 
 module.exports = router;
