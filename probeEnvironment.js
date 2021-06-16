6d6022c7d684b8928fb973904b239a34503525c2
Jonathan Sells <jsells@xcira.com>
2021-06-16 08:54:13 -0400

    added express application scaffolding

--- probeEnvironment.js
+++ probeEnvironment.js
@@ -1,6 +1,6 @@
+
+// Move into service 
 const { PythonShell } = require("python-shell");
-
-
 PythonShell.run('../python/CO2.py', null, function (err, results) {
   if (err) throw err;
   console.log('Results: %j', results);
