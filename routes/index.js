77e7a18a86ef52e354c417b1a162b98341a2e21a
Jonathan Sells <jsells@xcira.com>
2021-06-16 13:45:33 -0400

    sendfile

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 // just place the name of the view you want to render
 router.get('/', function(req, res, next) {
-  res.send('../views/index.html');
+  res.sendFile('../views/index.html');
 });
 
 module.exports = router;
