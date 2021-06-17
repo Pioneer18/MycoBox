a2135d5032dd9aa482d51414cf333151a22a6e42
Jonathan Sells <jsells@xcira.com>
2021-06-16 23:22:25 -0400

    applying event listeners

---
+++ public/javascripts/main_configuration.js
@@ -0,0 +1,22 @@
+function acceptDefaults () {
+    console.log("applying event listeners to accept_defaults")
+    // grab the accept_defaults radio buttons
+    var rad = document.main_config_form.accept_defaults;
+    console.log(rad);
+    var prev = null;
+    const overrides = document.getElementById('overrides');
+    // loop over both and addEventListener for change
+    for (var i = 0; i < rad.length; i++) {
+        rad[i].addEventListener('change', function() {
+            // defaults have been rejected
+            if (this !== 'Yes') {
+                overrides.classList.add('hidden');
+            } 
+            if (this == 'Yes') {
+                overrides.classList.remove('hidden');
+            }
+        });
+    }
+}
+// apply event listeners
+acceptDefaults();
