bb2bfaa3caee95c61693184e8778bce3aa3dfcc3
Jonathan Sells <jsells@xcira.com>
2021-06-16 23:49:17 -0400

    building out the main configuration

--- public/javascripts/main_configuration.js
+++ public/javascripts/main_configuration.js
@@ -1,19 +1,19 @@
 function acceptDefaults () {
     console.log("applying event listeners to accept_defaults")
     // grab the accept_defaults radio buttons
-    var rad = document.main_config_form.accept_defaults;
+    let rad = document.main_config_form.accept_defaults;
     console.log(rad);
-    var prev = null;
+    let prev = null;
     const overrides = document.getElementById('overrides');
     // loop over both and addEventListener for change
     for (let i = 0; i < rad.length; i++) {
         rad[i].addEventListener('change', function() {
             // defaults have been rejected
             console.log(rad[i]);
-            if ( rad[i].value !== 'Yes') {
+            if ( rad[i].value == 'Yes') {
                 overrides.classList.add('hidden');
             } 
-            if ( rad[i].value == 'Yes') {
+            if ( rad[i].value !== 'Yes') {
                 overrides.classList.remove('hidden');
             }
         });
