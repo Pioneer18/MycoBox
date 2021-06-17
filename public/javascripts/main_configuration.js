fb86de7d83d1f085d3e2e6b1f2a73105b7f1de6b
Jonathan Sells <jsells@xcira.com>
2021-06-17 00:28:53 -0400

    polishing

--- public/javascripts/main_configuration.js
+++ public/javascripts/main_configuration.js
@@ -1,4 +1,7 @@
+const default_configs = require('./process_config');
+let DEFAULTS = true;
 // Hide or Display the Overrides
+// pass default env config object for selected mushroom or map new env config
 function acceptDefaults () {
     console.log("applying event listeners to accept_defaults")
     let rad = document.main_config_form.accept_defaults;
@@ -9,11 +12,32 @@ function acceptDefaults () {
         rad[i].addEventListener('change', function() {
             if ( rad[i].value == 'Yes') {
                 overrides.classList.add('hidden');
+                DEFAULTS = true;
             } 
             if ( rad[i].value !== 'Yes') {
                 overrides.classList.remove('hidden');
+                DEFAULTS = false;
             }
         });
+    }
+}
+
+function startProces() {
+    processConfiguration()
+    // set the environmental variables
+ 
+}
+
+function processConfiguration() {
+    const form = document.main_config_form;
+    let process_settings;
+    console.log("Defaults: ", DEFAULTS);
+    // check for overrides
+    if (DEFAULTS) {
+        // select default_config for the selected mushroom
+        process_settings = default_configs[form.mushroom]
+        console.log(process_settings);
+
     }
 }
 
