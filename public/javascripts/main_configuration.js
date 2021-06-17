8a475d5613c81ddb8accc67a26b2af22bb4239a0
Jonathan Sells <jsells@xcira.com>
2021-06-17 00:46:28 -0400

    just using one file

--- public/javascripts/main_configuration.js
+++ public/javascripts/main_configuration.js
@@ -1,4 +1,65 @@
-const default_configs = require('./process_config');
+const default_configs = {
+    default_incubation_config: {
+        pink_oyster: {
+            temperature: '',
+            humidity: '',
+            co2: '',
+            lighting: false,
+            duration: 35 // days
+        },
+        golden_oyster: {
+            temperature: '',
+            humidity: '',
+            co2: '',
+            lighting: false,
+            duration: 35 // days
+        },
+        blue_oyster: {
+            temperature: '',
+            humidity: '',
+            co2: '',
+            lighting: false,
+            duration: 35 // days
+        },
+        shitake: {
+            temperature: '',
+            humidity: '',
+            co2: '',
+            lighting: false,
+            duration: 35 // days
+        }
+    },
+    default_fruiting_config: {
+        pink_oyster: {
+            temperature: '', // celsius
+            humidity: '',
+            co2: '', // ppm
+            lighting: true,
+            duration: 35 // days
+        },
+        golden_oyster: {
+            temperature: '', // celsius
+            humidity: '',
+            co2: '', // ppm
+            lighting: true,
+            duration: 35 // days
+        },
+        blue_oyster: {
+            temperature: '', // celsius
+            humidity: '',
+            co2: '', // ppm
+            lighting: true,
+            duration: 35 // days
+        },
+        shitake: {
+            temperature: '', // celsius
+            humidity: '',
+            co2: '', // ppm
+            lighting: true,
+            duration: 35 // days
+        }
+    }
+}
 let DEFAULTS = true;
 // Hide or Display the Overrides
 // pass default env config object for selected mushroom or map new env config
