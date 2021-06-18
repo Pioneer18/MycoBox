c061fdbf639c833b9c0a0a3013a00590c3ff8130
Jonathan Sells <jsells@xcira.com>
2021-06-17 22:25:09 -0400

    test.js is correct code for running a python subprocess shell

--- public/javascripts/subroutines/test.js
+++ public/javascripts/subroutines/test.js
@@ -1,17 +1,19 @@
-// require child_process.exc method
-const { spawn } = require('child_process');
 // spawn a python file to request CO2 reading
+const { spawn } = require('child_process');
 console.log('spawning the process now');
 const process = spawn('python', ["../../../python/CO2.py"]);
+
 // log stdout from the process
 process.stdout.on('data', function(data) {
     console.log(`stdout: ${data}`);
 })
 
+// log errors from the process
 process.stderr.on('data', (data) => {
     console.error(`stderr: ${data}`);
 });
   
+// log the exit code
 process.on('close', (code) => {
     console.log(`child process exited with code ${code}`);
 });
