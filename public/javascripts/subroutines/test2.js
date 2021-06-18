7ebcd78280284cf0e110b17ce01400526a00f5e5
Jonathan Sells <jsells@xcira.com>
2021-06-17 23:07:22 -0400

    const require not import

--- public/javascripts/subroutines/test2.js
+++ public/javascripts/subroutines/test2.js
@@ -1,4 +1,4 @@
-import { PythonShell } from "python-shell";
+const { PythonShell } = require("python-shell");
 
 PythonShell.run('../../../python/CO2.py', null, function(err, results) {
     if (err) throw err;
