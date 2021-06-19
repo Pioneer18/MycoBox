f23a62d62e4fd355cb36c5c0a2df7cd3ffe5a9af
Jonathan Sells <jsells@xcira.com>
2021-06-19 15:15:26 -0400

    setup the bin to run both applications

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -1,3 +1,23 @@
-/**
- * Serve Static Files and Mount View Controllers to the Server Routes
- */
+ var express = require('express');
+ var path = require('path');
+ var logger = require('morgan');
+ 
+ // Application Routers
+ 
+ 
+ // Server
+ var app = express();
+ 
+ // Mount Middleware
+ app.use(logger('dev'));
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: false }));
+ app.use(cookieParser());
+ app.use(express.static(path.join(__dirname, 'public')));
+ 
+ // Mount Application Routers
+//  app.use('/', indexRouter);
+//  app.use('/users', usersRouter);
+ 
+ module.exports = app;
+ 
