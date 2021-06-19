a4f93846ffbe69b1a0d25c43ea77ba069afb375f
Jonathan Sells <jsells@xcira.com>
2021-06-19 11:10:10 -0400

    cleared project to generate new architecture with no view engine

--- python/cozir-calibrate.py
+++
@@ -1,9 +0,0 @@
-import serial
-import time
-# Fresh Air Calibrate Cozir Sensor
-ser = serial.Serial("/dev/ttyAMA0")
-print ("Sending Calibrate Command\n")
-ser.write("G\r\n".encode())
-ser.reset_input_buffer()
-resp = ser.read(10)
-print("Sensor Response ",resp)
