860e67d2a6b4f79ecda6d0ba24e133164f73d0bc
Jonathan Sells <jsells@xcira.com>
2021-06-20 15:44:14 -0400

    streamlining the front-end router

--- FrontEnd/public/javascript/api/index.js
+++ FrontEnd/public/javascript/api/index.js
@@ -11,6 +11,11 @@ class FrontEndApi {
          */
         this.starProcess = function (config) {
             // create the XMLHTTP instance
+            this.makeRequest('POST', '/',{belh: 'blehh'}); // add a content-type param
+        };
+
+        this.makeRequest = (method, url, data) => {
+            // create the XMLHTTP instance
             let httpRequest = new XMLHttpRequest();
 
             if (!httpRequest) {
@@ -36,26 +41,8 @@ class FrontEndApi {
                 }
             }
             //httpRequest.setRequestHeader('Content-Type', 'application/json'); 
-            httpRequest.open('GET','/home/pi/RootCulture/test.html', true); // async true
-            httpRequest.send();
-        };
-
-        this.returnResponse = () => {
-            // process the server response here
-            if (httpRequest.readyState === XMLHttpRequest.DONE) {
-                // Everything is good, the response was received.
-                if (httpRequest.status === 200) {
-                    // Perfect!
-                    console.log('Below is the backend http response:')
-                    console.log(httpRequest.responseText);
-                } else {
-                    // There was a problem with the request.
-                    // For example, the response may have a 404 (Not Found)
-                    // or 500 (Internal Server Error) response code.
-                }
-            } else {
-                // Not ready yet.
-            }
+            httpRequest.open(method, url, true); // async true
+            httpRequest.send(data);
         }
     }
 }
