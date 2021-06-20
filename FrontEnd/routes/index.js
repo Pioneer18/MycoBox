12a81efd75e3305ad042d3fbaf717c32b052d144
Jonathan Sells <jsells@xcira.com>
2021-06-20 07:20:30 -0400

    correcting path to configuration.html

--- FrontEnd/routes/index.js
+++ FrontEnd/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname + '/FrontEnd/public/javascript/views/configuration.html'));
+  res.sendFile(path.join(__dirname + '../public/javascript/views/configuration.html'));
 });
 
 module.exports = router;
