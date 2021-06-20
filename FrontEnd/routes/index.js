d170791b16be0845b2bfcc12844079238473b57d
Jonathan Sells <jsells@xcira.com>
2021-06-20 07:25:53 -0400

    added root project path to indexRouter

--- FrontEnd/routes/index.js
+++ FrontEnd/routes/index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 
+const rootPath = "/home/pi/RootCulture/FrontEnd/public/javascript/views"
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile("../public/javascript/views/configuration.html");
+  res.sendFile(rootPath + "configuration.html");
 });
 
 module.exports = router;
