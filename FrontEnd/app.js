8459d2ab8fbd6e27a5d838b38f98ef5e5d10ba14
Jonathan Sells <jsells@xcira.com>
2021-06-20 10:23:52 -0400

    added and configured the jade view engine to render the html with all it's dependencies

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -1,22 +1,25 @@
- var express = require('express');
- var path = require('path');
- var logger = require('morgan');
- 
- // Application Routers
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+
+// Application Routers
 const indexRouter = require('./routes/index');
- 
- // Server
- var app = express();
- 
- // Mount Middleware
- app.use(logger('dev'));
- app.use(express.json());
- app.use(express.urlencoded({ extended: false }));
- app.use(express.static(path.join(__dirname, 'public')));
- 
- // Mount Application Routers
-  app.use('/', indexRouter);
+
+// Server
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, '/public/javascript/views'));
+app.set('view engine', 'jade');
+
+// Mount Middleware
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Mount Application Routers
+app.use('/', indexRouter);
 //  app.use('/users', usersRouter);
- 
- module.exports = app;
- 
+
+module.exports = app;
--- FrontEnd/routes/index.js
+++ FrontEnd/routes/index.js
@@ -2,11 +2,9 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 
-const rootPath = "/home/pi/RootCulture/FrontEnd/public/javascript/views"
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.location(rootPath + "/configuration/configuration.html");
+  res.render('configuration/configuration.jade')
 });
 
 module.exports = router;
