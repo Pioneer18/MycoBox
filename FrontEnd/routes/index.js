27a8d8f3488456b6d2ac23a70fa2c737b7241681
Jonathan Sells <jsells@xcira.com>
2021-06-20 10:51:35 -0400

    not defining file type for view engine

--- FrontEnd/routes/index.js
+++ FrontEnd/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('configuration/configuration.jade')
+  res.render('configuration/configuration')
 });
 
 module.exports = router;
