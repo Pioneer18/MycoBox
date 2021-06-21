7edf92f8ec4146f02f81fe13b9d669c0a974721a
Jonathan Sells <jsells@xcira.com>
2021-06-21 08:27:32 -0400

    small update, removed users router

--- MycoBox/routes/index.js
+++ MycoBox/routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+/**
+ * Route HTTP requests to correct controllers
+ */
 
 /* Start Process */
 router.post('/', function(req, res, next) {
