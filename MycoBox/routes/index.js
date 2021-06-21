9ca6341e2b7ff9bff507227d9021e98688a265c7
Jonathan Sells <jsells@xcira.com>
2021-06-21 19:19:13 -0400

    require()'

--- MycoBox/routes/index.js
+++ MycoBox/routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 // The Controller Classes need to be instantiated before getting here...
-import {startProcess} from '../controllers/system.controller';
+const { startProcess } = require('../controllers/system.controller');
 /**
  * Route HTTP requests to correct controllers
  */
 
 /* Start Process */
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
     res.send(JSON.stringify(startProcess()));
     res.send('Should Have recieved a JSON');
 });
