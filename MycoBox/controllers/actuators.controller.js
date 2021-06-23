0765c02a9a09086ad60b12c8a1fb2eba6c347123
Jonathan Sells <jsells@xcira.com>
2021-06-22 23:12:50 -0400

    reoragnizing the backend

---
+++ MycoBox/controllers/actuators.controller.js
@@ -0,0 +1,4 @@
+/**
+ * Acutators Controller
+ * This controller is responsible for calling the actuator modules' avaiable actions for updating the MycoBox Environment Model
+ */
--- MycoBox/services/system.service.js
+++ MycoBox/services/system.service.js
@@ -1,3 +1,17 @@
 /**
- * This service runs a MycoBox Session
+ * System Service
  */
+
+module.exports = {
+
+    /**
+     * New Session
+     * @param process
+     * @param config
+     */
+    newSession: (process, config) => {
+        //call the prepare_environment subroutine to set the Evironment Model to the config
+    }
+
+}
+
