517caaeb5bb99a3e38381b12b960147c228b26ed
Jonathan Sells <jsells@xcira.com>
2021-06-24 08:49:44 -0400

    trying to get a read

--- test2.js
+++ test2.js
@@ -6,12 +6,6 @@ port.write("K 2\r\n");
 setTimeout(() => {
     console.log('pause a second')
 }, 1000);
-port.on('readable', () => {
-    console.log('Data: ', port.read());
-});
-port.on('data', (data)=> {
-    console.log('Data: ', data);
-});
 const lineStream = port.pipe(new Readline({ delimiter: '\r\n' }));
 lineStream.on('data', (data)=> {
     console.log(data);
