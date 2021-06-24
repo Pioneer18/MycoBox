f0052b462302dadd092bb9f20f389411838665cb
Jonathan Sells <jsells@xcira.com>
2021-06-24 09:01:30 -0400

    simple?

--- test2.js
+++ test2.js
@@ -1,12 +1,30 @@
+/**
+ * Events:
+ * 
+ * open: on port open 
+ * 
+ * close: on port close
+ * 
+ * data: listening for data, puts port in flowing mode. Data is emitted as soon as it arrives; it's a buffer
+ * 
+ * drain:
+ */
+
+/**
+ * Methods:
+ * 
+ * write: write data, buffered if receiving port is not open yet
+ * 
+ * read: request a number of bytes from the SerialPort, it pulls the data from the internal buffer
+ * 
+ * flush: wipe unwritten or read data
+ */
+
 const SerialPort = require('serialport');
 const Readline = SerialPort.parsers.Readline;
 const port = new SerialPort('/dev/ttyAMA0');
 port.write("M 4\r\n");
 port.write("K 2\r\n");
-setTimeout(() => {
-    console.log('pause a second')
-}, 1000);
-const lineStream = port.pipe(new Readline({ delimiter: '\r\n' }));
-lineStream.on('data', (data)=> {
-    console.log(data);
-})
+const data = port.read(10);
+console.log(data);
+
