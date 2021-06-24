fb2017efa739616c8826c749ae9835aafbf19bd3
Jonathan Sells <jsells@xcira.com>
2021-06-24 08:39:18 -0400

    magic?

--- test2.js
+++ test2.js
@@ -2,6 +2,9 @@ const SerialPort = require('serialport');
 const Readline = SerialPort.parsers.Readline;
 const port = new SerialPort('/dev/ttyAMA0');
 const parser = port.pipe(new Readline());
-parser.on('data', console.log);
 port.write("M 4\r\n");
 port.write("K 2\r\n");
+setTimeout(() => {
+    console.log('pause a second')
+}, 1000);
+parser.on('data', console.log);
