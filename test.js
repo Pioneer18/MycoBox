60bc2ce4da74aaaca98c49588d27568738f3a6db
Jonathan Sells <jsells@xcira.com>
2021-06-24 01:06:02 -0400

    shouldn't be anything coming through, but if no errors this is good

--- test.js
+++ test.js
@@ -10,6 +10,19 @@ port.write('main screen turn on', function (err) {
     console.log('message written')
 })
 
+// Read data that is available but keep the stream in "paused mode"
+port.on('readable', function () {
+    console.log('Data:', port.read())
+  })
+  
+  // Switches the port into "flowing mode"
+  port.on('data', function (data) {
+    console.log('Data:', data)
+  })
+  
+  // Pipe the data into another stream (like a parser or standard out)
+  const lineStream = port.pipe(new Readline())
+
 // Open errors will be emitted as an error event
 port.on('error', function (err) {
     console.log('Error: ', err.message)
