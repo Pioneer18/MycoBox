e21876edf98a8215659767749cf8e6f241a3c9ae
Jonathan Sells <jsells@xcira.com>
2021-06-24 08:28:01 -0400

    testing serialport write and read in js

---
+++ test2.js
@@ -0,0 +1,10 @@
+const SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline;
+const port = new SerialPort('dev/ttyAMA0');
+const parser = port.pipe(new Readline());
+parser.on('data', console.log);
+port.write("M 4\r\n");
+port.write("K 2\r\n");
+setTimeout(() => {
+    console.log('paused a second')
+}, 1000);
