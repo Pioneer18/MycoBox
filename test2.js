2eae7155b7d9864469c11507ef07af74eb7478cb
Jonathan Sells <jsells@xcira.com>
2021-06-24 08:43:43 -0400

    trying to get a read

--- test2.js
+++ test2.js
@@ -1,10 +1,17 @@
 const SerialPort = require('serialport');
 const Readline = SerialPort.parsers.Readline;
 const port = new SerialPort('/dev/ttyAMA0');
-const parser = port.pipe(new Readline());
 port.write("M 4\r\n");
 port.write("K 2\r\n");
 setTimeout(() => {
     console.log('pause a second')
 }, 1000);
 parser.on('data', console.log);
+port.on('readable', () => {
+    console.log('Data: ', port.read());
+});
+port.on('data', (data)=> {
+    console.log('Data: ', data);
+});
+const lineStream = port.pipe(new Readline());
+console.log(lineStream);
