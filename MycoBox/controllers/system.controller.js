4fed2e81b1e33fb894b6d230b51adf5c17179f70
Jonathan Sells <jsells@xcira.com>
2021-07-09 21:17:46 -0400

    changing notes in the system.controller.js file

--- MycoBox/controllers/system.controller.js
+++ MycoBox/controllers/system.controller.js
@@ -1,18 +1,25 @@
 /**
  * System Controller
  * ===General=================
- * - New Session:
- * - End Session:
- * - Add Hours:
- * - Subtract Hours:
+ * - New Session: start a new session that will last for a given duration
+ * - End Session: end the session and redirect to post session page (go through 'ending process')
+ * - Add Hours: add hours to the duration
+ * - Subtract Hours: subtract hours from the duration
  * ===Environment================
- * - Increase temp
- * - Decrease temp
- * - Increase co2
- * - Decrease co2
- * - Increase humidity
- * - Decrease humidity
- *  note: incubation vs fruiting and other actuator dependent rules
+ * - ManageEnvironment:
+ *   - read the current environment config
+ *   - uses sensor controller to read the current environment (update environment model)
+ *   - if environment does not match config, 
+ *     * set Environment Status (ES): 'Updating' (instead of 'Set')
+ *     * set the environment with setEnvironment Method, till completed keep ES as 'Updating' // till true is returned
+ *     * once the environment is set update the ES to true
+ *   - if environment matches the config,
+ *     * set timer for 15 seconds and check and repeat
+ * 
+ * During a session, the manageEnvironment function receives an "updated environment configuration" whenever a user requests to change something in the environment.
+ * This implies that Frontend control panel requests to change the environment all update the environment configuration, and then the manageEnvironement function will
+ * handle the update; by calling on the appropriate controllers for updating the environment.
+ *  
  */
 module.exports = {
 
