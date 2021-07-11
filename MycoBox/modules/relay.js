98d2e0c70bf254c1b7d646f9e8aaa53449b84ec1
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-10 23:13:57 -0400

    created the relay module

--- MycoBox/modules/relay.js
+++ MycoBox/modules/relay.js
@@ -1,5 +1,6 @@
 /**
  * Relay Module:
+ * provides methods for switching the 12 relays on/off
  * 
  */
 const Gpio = require('onoff').Gpio;
@@ -16,7 +17,127 @@ module.exports = {
     s1r1_off: () => {
         const relay = new Gpio(0, 'out');
         relay.writeSync(1)
-    }
+    },
 
+    // Heater
+    s2r1_on: () => {
+        const relay = new Gpio()
+        relay.writeSync(0)
+    },
+
+    s2r1_off: () => {
+        const relay = new Gpio()
+        relay.writeSync(1)
+    },
+
+    // Humidifier
+    s3r1_on: () => {
+        const relay = new Gpio()
+        relay.writeSync(0)
+    },
+
+    s3r1_off: () => {
+        const relay = new Gpio()
+        relay.writeSync(1)
+    },
+
+    // Intake Fan
+    s4r1_on: () => {
+        const relay = new Gpio()
+        relay.writeSync(0)
+    },
+
+    s4r1_off: () => {
+        const relay = new Gpio()
+        relay.writeSync(1)
+    },
+
+    // Realy 2 1
+    s1r2_on: () => {
+        const relay = new Gpio()
+        relay.writeSync(0)
+    },
+
+    s1r2_off: () => {
+        const relay = new Gpio()
+        relay.writeSync(1)
+    },
+
+    // Relay 2 2
+    s2r2_on: () => {
+        const relay = new Gpio()
+        relay.writeSync(0)
+    },
+
+    s2r2_off: () => {
+        const relay = new Gpio()
+        relay.writeSync(1)
+    },
+
+    // Relay 2 3
+    s3r2_on: () => {
+        const relay = new Gpio()
+        relay.writeSync(0)
+    },
+
+    s3r2_off: () => {
+        const relay = new Gpio()
+        relay.writeSync(1)
+    },
+
+    // Relay 2 4
+    s4r2_on: () => {
+        const relay = new Gpio()
+        relay.writeSync(0)
+    },
+
+    s4r2_off: () => {
+        const relay = new Gpio()
+        relay.writeSync(1)
+    },
+
+    // Relay 2 5
+    s5r2_on: () => {
+        const relay = new Gpio()
+        relay.writeSync(0)
+    },
+
+    s5r2_off: () => {
+        const relay = new Gpio()
+        relay.writeSync(1)
+    },
+
+    // Relay 2 6
+    s6r2_on: () => {
+        const relay = new Gpio()
+        relay.writeSync(0)
+    },
+
+    s6r2_off: () => {
+        const relay = new Gpio()
+        relay.writeSync(1)
+    },
+
+    // Relay 2 7
+    s7r2_on: () => {
+        const relay = new Gpio()
+        relay.writeSync(0)
+    },
+
+    s7r2_off: () => {
+        const relay = new Gpio()
+        relay.writeSync(1)
+    },
+
+    // Relay 2 8
+    s8r2_on: () => {
+        const relay = new Gpio()
+        relay.writeSync(0)
+    },
+
+    s8r2_off: () => {
+        const relay = new Gpio()
+        relay.writeSync(1)
+    },
 
 }
