96c2161c22c87cebc0e4ae4eb56a2d36ee4fae06
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-10 20:40:44 -0400

    creating a relay module

--- MycoBox/modules/relay.js
+++ MycoBox/modules/relay.js
@@ -1,6 +1,24 @@
 /**
- * Module for switching the physical relay
+ * Relay Module:
+ * 
  */
 const Gpio = require('onoff').Gpio;
 const IN4R1 = new Gpio(13, 'out');
 IN4R1.writeSync(0);
+
+module.exports = {
+   /* Relay Module 1 */
+
+   // Air Conditioner
+   s1r1_on = () => {
+       const relay = new Gpio(0, 'out');
+       relay.writeSync(0)
+   },
+
+   s1r1_off = () => {
+    const relay = new Gpio(0, 'out');
+    relay.writeSync(1)
+}
+
+    
+}
