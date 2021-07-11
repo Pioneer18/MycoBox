47c5677a1e7f857c0a5a067ba78131bf0437a0c7
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-10 21:32:25 -0400

    trying to run from module

--- MycoBox/modules/relay.js
+++ MycoBox/modules/relay.js
@@ -3,17 +3,15 @@
  * 
  */
 const Gpio = require('onoff').Gpio;
-const IN4R1 = new Gpio(0, 'out');
-IN4R1.writeSync(0);
 
 module.exports = {
-    //    /* Relay Module 1 */
+    /* Relay Module 1 */
 
-    //    // Air Conditioner
-    //    s1r1_on:() => {
-    //        const relay = new Gpio(0, 'out');
-    //        relay.writeSync(0)
-    //    },
+    // Air Conditioner
+    s1r1_on:() => {
+        const relay = new Gpio(0, 'out');
+        relay.writeSync(0)
+    },
 
     s1r1_off: () => {
         const relay = new Gpio(0, 'out');
