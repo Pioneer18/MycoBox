805a45a991f6cc2a6e2d7614288d10352a2107e4
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-10 20:42:52 -0400

    updates

--- MycoBox/modules/relay.js
+++ MycoBox/modules/relay.js
@@ -10,12 +10,12 @@ module.exports = {
    /* Relay Module 1 */
 
    // Air Conditioner
-   s1r1_on = () => {
+   s1r1_on:() => {
        const relay = new Gpio(0, 'out');
        relay.writeSync(0)
    },
 
-   s1r1_off = () => {
+   s1r1_off: () => {
     const relay = new Gpio(0, 'out');
     relay.writeSync(1)
 }
