5713952281ce6a9c188fa74694e10c744f5a43e2
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-10 21:16:29 -0400

    test

--- MycoBox/modules/relay.js
+++ MycoBox/modules/relay.js
@@ -2,9 +2,9 @@
  * Relay Module:
  * 
  */
-// const Gpio = require('onoff').Gpio;
-// const IN4R1 = new Gpio(13, 'out');
-// IN4R1.writeSync(0);
+const Gpio = require('onoff').Gpio;
+const IN4R1 = new Gpio(0, 'out');
+IN4R1.writeSync(0);
 
 module.exports = {
     //    /* Relay Module 1 */
