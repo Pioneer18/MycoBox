a1e8d11eec5a6e78cf9a2bd636725fc1412691ee
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-11 20:59:53 -0400

    install packages for creating the cli program and start the index file

---
+++ MycoBox/cli_control_panel/index.js
@@ -0,0 +1,7 @@
+/**
+ * MycoBox CLI Overrides:
+ * Purpose: Program for controling each actuator's override switch, but may become cli application
+ * for starting and controlling a MycoBox Session! Yep, even with the internet down or no electronic
+ * device besides the MycoBox itself, if you have a keyboard you can run a session and still locally
+ * log all session data. It can be uploaded later (auto) or stored on the external memory (SSD)
+ */
