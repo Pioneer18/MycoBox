932bb40a1903707177f75b8be7f8523d259c728b
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-13 12:49:21 -0400

    removing old hx711 code

--- MycoBox/controllers/index.js
+++ MycoBox/controllers/index.js
@@ -1,8 +1,10 @@
 /**
  * MycoBox Controllers index
  */
-const {SystemController} = require('./system.controller');
+const SystemController = require('./system.controller');
+const TemperatureController = require('../controllers/temperature.controller');
 
 module.exports = {
     systemController: SystemController,
+    temperatureController: TemperatureController,
 }
