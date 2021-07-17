bc362ae05907025d0957eac937fe384f24409a9a
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-17 19:49:32 -0400

    imported globals into the system.service

--- MycoBox/services/system.service.js
+++ MycoBox/services/system.service.js
@@ -3,10 +3,17 @@
  * =========================
  * Description: provides extra functionality to the system controller
  */
+const {
+    sessionConfig,
+    environmentConfig,
+    environmentModel,
+    systemStatus,
+    overrides
+} = require('../globals/globals');
 
 module.exports = {
 
     manageEnvironment: () => {
-        
+
     }
 }
