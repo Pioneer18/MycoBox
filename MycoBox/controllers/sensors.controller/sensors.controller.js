0b7cff27e2e0a63be1fe396476022556fe0fe255
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-18 09:59:55 -0400

    building out the sensors controller to request from each group of sensors and return a value

---
+++ MycoBox/controllers/sensors.controller/sensors.controller.js
@@ -0,0 +1,57 @@
+/**
+ * Sensors Controller
+ */
+const {PythonShell} = require('python-shell')
+let options = {
+    mode: 'text',
+    pythonOptions: ['-u'], // get print results in real-time
+    scriptPath: '../../python',
+    // args: ['value1', 'value2', 'value3']
+  };
+
+module.exports = {
+    /**
+     * Temperature readings - internal & external
+     * @param {Array} reply []
+     */
+    readTemp: () => {
+
+    },
+
+    /**
+     * CO2 readings from COZIR-A sensor
+     * @param {Array} reply ["CO2 PPM = 536.0"]
+     */
+    readCO2: ()=> {
+        PythonShell.run('CO2.py', options, function (err, reply) {
+            if (err) throw err;
+            console.log('reply: %j', reply);
+            return reply;
+        });
+    },
+
+    /**
+     * Humidity readings - internal & external
+     * @param {Array} reply
+     */
+    readHumidity: () => {
+
+    },
+
+    /**
+     * Weight readings from Esp8266 scale ** HTTP or Serial request
+     * @param {Array} reply
+     */
+    readScale: () => {
+
+    },
+
+    /**
+     * Infrared Temp readings - grow bags
+     * @param {Array} reply
+     */
+    readInfrared: () => {
+        
+    }
+
+}
