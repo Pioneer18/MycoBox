1555d75fa4758a87f443364b7e950248ce846013
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-17 21:52:38 -0400

    updating the setEnvironmentModel method

--- MycoBox/controllers/sensors.controller.js
+++ MycoBox/controllers/sensors.controller.js
@@ -2,10 +2,20 @@
  * Sensors Controller
  */
 const {PythonShell} = require('python-shell')
-let pyshell = new PythonShell('')
+let options = {
+    mode: 'text',
+    pythonOptions: ['-u'], // get print results in real-time
+    scriptPath: '../python',
+    // args: ['value1', 'value2', 'value3']
+  };
+
 module.exports = {
     // return a reading from every sensor
     setEnvironmentModel: ()=> {
-
+        PythonShell.run('CO2.py', options, function (err, results) {
+            if (err) throw err;
+            // results is an array consisting of messages collected during execution
+            console.log('results: %j', results);
+        });
     }
 }
