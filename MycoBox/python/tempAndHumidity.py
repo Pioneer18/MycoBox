d05157c7a9e2766dd1540f45ef27301e1c358ae5
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-18 11:59:49 -0400

    fix

--- MycoBox/python/tempAndHumidity.py
+++ MycoBox/python/tempAndHumidity.py
@@ -8,7 +8,7 @@ success_1 = False
 success_2 = False
 success_3 = False
 count = 0
-while success_1 is False or success_2 is False or success_3 is False and count < 10:
+while success_1 is False or success_2 is False or success_3 is False:
     h1, temp_1 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_1)
     h2, temp_2 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_2)
     h3, temp_3 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_3)
@@ -21,7 +21,7 @@ while success_1 is False or success_2 is False or success_3 is False and count <
     if h3 is not None and temp_3 is not None:
         print("Temp_3={0:0.1f}*C Humidity_3={1:0.1f}%")
         success_3 = True
-    if count < 10:
-        count += 1
+    # if count < 10:
+    #     count += 1
     else:
         print("Failed to retrieve data from humidity sensor")
