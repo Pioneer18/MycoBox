0e7685d54701299eb82d7d879a538bb91ad33523
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-18 13:29:22 -0400

    test

--- MycoBox/controllers/sensors.controller/sensors.controller.test.js
+++ MycoBox/controllers/sensors.controller/sensors.controller.test.js
@@ -1,3 +1,2 @@
-const {setEnvironmentModel, read_temp_humidity} = require('./sensors.controller')
-// setEnvironmentModel();
-setEnvironmentModel()
+const {set_environment_model, read_temp_humidity} = require('./sensors.controller')
+set_environment_model()
