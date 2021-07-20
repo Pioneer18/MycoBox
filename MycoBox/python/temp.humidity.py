b71090d0b740463ad5df3b2a66d6a6643635ea08
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-19 22:34:00 -0400

    using easy to parse string to report sensor readings

--- MycoBox/python/temp.humidity.py
+++ MycoBox/python/temp.humidity.py
@@ -5,30 +5,16 @@ DHT_1 = 24  # lower dht22
 DHT_2 = 25  # top dht22
 DHT_3 = 1  # external dht22
 sensor_list = [1, 2]
-results = {
-    "h1": 0,
-    "h2": 0,
-    "temp1": 0,
-    "temp2": 0,
-}
-    # "h3": 0,
-    # "temp3": 0,
+h1=0,h2=0,h3=0,temp1=0,temp2=0,temp3=0
 for x in sensor_list:
     if x == 1:
-        h1, temp_1 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_1)
-        # results.update({"h1": h1})
-        # results.update({"temp1": temp_1})
-        print('h1={', h1,'}')
+        h1, temp1 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_1)
         continue
     if x == 2:
-        h2, temp_2 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_2)
-        # results.update({"h2": h2})
-        # results.update({"temp2": temp_2})
+        h2, temp2 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_2)
         continue
-    # if x == 3:
-    #     h3, temp_3 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_3)
-    #     results.update({"h3": h3})
-    #     results.update({"temp3": temp_3})
-    #     continue
-print(results)
+    if x == 3:
+        h3, temp_3 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_3)
+        continue
+print('h1={',h1,'} h2={',h2,'} h3={',h3,'} temp1={',temp1,'} temp2={', temp2,'} temp3={',temp3,'}')
 
