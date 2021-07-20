58c91788b73ce8dac91668c36df28d887809d5f2
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-19 22:35:26 -0400

    using easy to parse string to report sensor readings

--- MycoBox/python/temp.humidity.py
+++ MycoBox/python/temp.humidity.py
@@ -5,7 +5,12 @@ DHT_1 = 24  # lower dht22
 DHT_2 = 25  # top dht22
 DHT_3 = 1  # external dht22
 sensor_list = [1, 2]
-h1=0,h2=0,h3=0,temp1=0,temp2=0,temp3=0
+h1 = 0
+h2 = 0
+h3 = 0
+temp1 = 0
+temp2 = 0
+temp3 = 0
 for x in sensor_list:
     if x == 1:
         h1, temp1 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_1)
@@ -16,5 +21,5 @@ for x in sensor_list:
     if x == 3:
         h3, temp_3 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_3)
         continue
-print('h1={',h1,'} h2={',h2,'} h3={',h3,'} temp1={',temp1,'} temp2={', temp2,'} temp3={',temp3,'}')
-
+print('h1={', h1, '} h2={', h2, '} h3={', h3, '} temp1={',
+      temp1, '} temp2={', temp2, '} temp3={', temp3, '}')
