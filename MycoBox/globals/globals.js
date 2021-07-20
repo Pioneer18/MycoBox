52da68033e72f988e46e6a80632797cd06b51dae
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-20 09:09:17 -0400

    testing process.env

--- MycoBox/globals/globals.js
+++ MycoBox/globals/globals.js
@@ -1,50 +1,50 @@
-global.sessionConfig = {
-    active_session: '', // checked by newSession method only once
-    session_id: '', // for logs
-    user_id: '', // for logs
-    duration: '',
-    canceled: false, // will end the session
-}
-global.environmentConfig = {
-    temp: '',
-    irTemp: '',
-    humidity: '',
-    co2: '',
-    lighting: ''
-    // circulation_top: '',
-    // circulation_bottom: '',
-    // full_circulation: '',
-}
-global.systemStatus = {
-    /* ------------------ Actuators (on/off) ------------------  */
-    circulation_bottom: false, // 1
-    circulation_top: false, // 2
-    intake: false, // 3
-    exhaust: false, // 4
-    ac: false, // 5
-    heater: false, // 6 
-    humidifier: false, // 7
-    mb_light_1: false, // 8
-    mb_light_2: false, // 9
-    ib_light: false, // 10
-    speakers: false // 11
-}
-global.overrides = {
-    /* ------------------ Overrieds (on/off) ------------------ */
-    flag: false, // must be true for overrides to be honored
-    circulation_bottom: false,
-    circulation_top: false,
-    intake: false,
-    exhaust: false,
-    ac: false,
-    heater: false,
-    mister: false,
-    humidifier: false,
-    mb_light_1: false,
-    mb_light_2: false,
-    ib_light: false,
-    speakers: false,
-}
+// Session Config
+process.env.active_session = '', // checked by newSession method only once
+process.env.session_id = '', // for logs
+process.env.user_id = '', // for logs
+process.env.duration = '',
+process.env.canceled = false, // will end the session
+
+// Envrionment Config
+process.env.temp = '',
+process.env.irTemp = '',
+process.env.humidity = '',
+process.env.co2 = '',
+process.env.lighting = ''
+// circulation_top: '',
+// circulation_bottom: '',
+// full_circulation: '',
+
+
+// Actuators' Status
+process.env.circulation_bottom = false, // 1
+process.env.circulation_top = false, // 2
+process.env.intake = false, // 3
+process.env.exhaust = false, // 4
+process.env.ac = false, // 5
+process.env.heater = false, // 6 
+process.env.humidifier = false, // 7
+process.env.mb_light_1 = false, // 8
+process.env.mb_light_2 = false, // 9
+process.env.ib_light = false, // 10
+process.env.speakers = false // 11
+
+
+// Overrides
+process.env.flag = false // must be true for overrides to be honored
+process.env.circulation_bottom = false
+process.env.circulation_top = false
+process.env.intake = false
+process.env.exhaust = false
+process.env.ac = false
+process.env.heater = false
+process.env.mister = false
+process.env.humidifier = false
+process.env.mb_light_1 = false
+process.env.mb_light_2 = false
+process.env.ib_light = false
+process.env.speakers = false
+
 // Environment Model
 process.env.internal_temp_1 = '' // dht22_1
 process.env.internal_temp_2 = '' // dht22_2
