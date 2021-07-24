f1f2e1af1e9c6655ae8a1840313ef9d1e8c199d0
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-24 17:26:54 -0400

    test

--- MycoBox/python/temp.humidity.py
+++ MycoBox/python/temp.humidity.py
@@ -5,13 +5,14 @@ DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_1 = 24  # lower dht22
 DHT_2 = 25  # top dht22
 DHT_3 = 1  # external dht22
-sensor_list = [1,2]
+sensor_list = [1,2,3]
 h1 = 0
 h2 = 0
 h3 = 0
 temp1 = 0
 temp2 = 0
 temp3 = 0
+
 for x in sensor_list:
     if x == 1:
         h1, temp1 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_1)
@@ -21,6 +22,6 @@ for x in sensor_list:
         continue
     if x == 3:
         h3, temp_3 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_3)
-        continue
+
 print('h1={', h1, '} h2={', h2, '} h3={', h3, '} temp1={',
       temp1, '} temp2={', temp2, '} temp3={', temp3, '}')
