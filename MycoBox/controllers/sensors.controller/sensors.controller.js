85691d191100ab0c3c46ea0e3b83aba093d81fd8
Jonathan Sells <pioneer@jonathans-air.home>
2021-07-27 21:41:27 -0400

    starting to clean up the sensors.controller.js file and move more functionality to the utilities.js...which likely should just be the sensors.controller.service.js in hindsight

--- MycoBox/controllers/sensors.controller/sensors.controller.js
+++ MycoBox/controllers/sensors.controller/sensors.controller.js
@@ -3,7 +3,6 @@
  */
 const fs = require('fs')
 const { PythonShell } = require('python-shell');
-const { logger } = require('../../logger');
 const {
     parse_th_data,
     validate_sensor_data,
@@ -23,13 +22,6 @@ let options = {
 const read_temp_humidity = () => {
 
     PythonShell.run('temp.humidity.py', options, function (err, reply) {
-        // fs.chmod("example.txt", 0o600, () => {
-        //     console.log("Trying to write to file");
-        //     fs.writeFileSync('example.txt', "This file has now been edited.");
-    
-        //     console.log("\nReading the file contents");
-        //     console.log(fs.readFileSync("example.txt", 'utf8'));
-        // });
         if (err) throw err;
         const parsed = parse_th_data(reply)
         validate_sensor_data(parsed)
@@ -41,12 +33,6 @@ const read_temp_humidity = () => {
         console.log(parsed[5])
         console.log(parsed[6])
         console.log(parsed[7])
-        //logger.log('info', 'internal_humidity_1', parsed[0])
-        // internal_humidity_2: parsed[1],
-        // external_humidity: parsed[2],
-        // internal_temp_1: parsed[3],
-        // internal_temp_2: parsed[4],
-        // external_temp: parsed[5]
         return
     })
 }
@@ -99,18 +85,8 @@ const read_infrared = () => {
  * Set Environment Model - return readings of every sensor group
  */
 const set_environment_model = () => {
-    const test = read_temp_humidity()
-    const test1 = read_precise_temp()
-    // const test2 = await read_co2()
-    // console.log(process.env.internal_humidity_1)
-    // console.log(process.env.internal_humidity_2)
-    // console.log(process.env.external_humidity)
-    // console.log(process.env.internal_temp_1)
-    // console.log(process.env.internal_temp_2)
-    // console.log(process.env.external_temp)
-    // console.log(process.env.co2)
-    // console.log(process.env.precise_temp_c)
-    // console.log(process.env.precise_temp_f)
+    read_temp_humidity()
+    read_precise_temp()
     return
 }
 
