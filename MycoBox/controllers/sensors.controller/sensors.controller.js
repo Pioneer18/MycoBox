a46deb0447f539513e55965c278c1228031c672e
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-08 19:57:23 -0400

    debugging missing values

--- MycoBox/controllers/sensors.controller/sensors.controller.js
+++ MycoBox/controllers/sensors.controller/sensors.controller.js
@@ -68,7 +68,6 @@ const read_infrared = () => {
  * Set Environment Model
  */
 const set_environment_model = () => {
-    console.log(process.cwd())
     read_temp_humidity()
     read_precise_temp()
     read_scale()
@@ -79,21 +78,23 @@ const set_environment_model = () => {
 /**
  * Read Environment Model
  */
-const read_environment_model = () => {
-    return {
-        internal_temp_1: process.env.internal_temp_1,  
-        internal_temp_2: process.env.internal_temp_2,  
-        internal_temp_3: process.env.internal_temp_3,  
-        precise_temp_c: process.env.precise_temp_c, 
-        precise_temp_f: process.env.precise_temp_f, 
-        external_temp: process.env.external_temp, 
-        internal_humidity_1: process.env.internal_humidity_1, 
-        internal_humidity_2: process.env.internal_humidity_2, 
-        internal_humidity_3: process.env.internal_humidity_3, 
-        external_humidity: process.env.external_humidity, 
-        co2: process.env.co2, 
-        weight: process.env.weight, 
+const read_environment_model = async () => {
+    console.log('Getting Started Reading the EnvModel Now')
+    const envmodel = {
+        internal_temp_1: process.env.internal_temp_1,
+        internal_temp_2: process.env.internal_temp_2,
+        internal_temp_3: process.env.internal_temp_3,
+        precise_temp_c: process.env.precise_temp_c,
+        precise_temp_f: process.env.precise_temp_f,
+        external_temp: process.env.external_temp,
+        internal_humidity_1: process.env.internal_humidity_1,
+        internal_humidity_2: process.env.internal_humidity_2,
+        internal_humidity_3: process.env.internal_humidity_3,
+        external_humidity: process.env.external_humidity,
+        co2: process.env.co2,
+        weight: process.env.weight
     }
+    return await envmodel;
 }
 
 module.exports = {
--- MycoBox/routes/index.js
+++ MycoBox/routes/index.js
@@ -13,7 +13,8 @@ const api = "/api_v1";
  */
 router.get(api + '/sensors_controller/read_environment_model', function (req, res, next) {
     set_environment_model()
-    res.send(JSON.stringify(read_environment_model()))
+    console.log('Finsished setting EnvModel')
+    res.send(JSON.stringify(await read_environment_model()))
 })
 
 /**
