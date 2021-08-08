f0015f1d49ee6980457363dad35e38ba1bf37228
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-08 16:06:34 -0400

    test again

--- FrontEnd/public/javascript/controllers/dashboard/dashboard.js
+++ FrontEnd/public/javascript/controllers/dashboard/dashboard.js
@@ -6,9 +6,9 @@ console.log('Starting the Dashboard Controller')
 const insert_sensor_values = (values) => {
     for (const val in values) {
         console.log(val)
-        if(val === 'temp') document.getElementById('temp_field').innerHTML = '35C';
-        if(val === 'humidity') document.getElementById('humidity_field').innerHTML = '95%';
-        if(val === 'co2') document.getElementById('co2_field').innerHTML = '15,000ppm';
+        if(val === 'temp') document.getElementById('temp_field').innerHTML = val;
+        if(val === 'humidity') document.getElementById('humidity_field').innerHTML = val;
+        if(val === 'co2') document.getElementById('co2_field').innerHTML = val;
     }
 }
 const values = {'temp': 35, 'humidity': 99,'co2': 15000}
