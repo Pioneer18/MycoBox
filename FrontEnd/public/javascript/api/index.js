633168bef9c011cc90d6e394d6567c834492f53e
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-08 19:11:47 -0400

    testing api_v1 on routes

--- FrontEnd/public/javascript/api/index.js
+++ FrontEnd/public/javascript/api/index.js
@@ -13,7 +13,7 @@ class FrontEndApi {
          */
         this.newSession = (process, config) => {
             // create the XMLHTTP instance
-            this.makeRequest('POST', this.api + 'http://192.168.1.97:3000/system_controller/start_process', JSON.stringify({process, config})) // add a content-type param
+            this.makeRequest('POST',`http://192.168.1.97:3000${this.api }/system_controller/start_process`, JSON.stringify({process, config})) // add a content-type param
         };
 
         /**
