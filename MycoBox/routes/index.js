4ce5ff4883463ca1ee3947d15fd9b347734a9c4c
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-08 11:49:23 -0400

    removed logs from Frontend and fixed import url for system.controller in myco_server router

--- MycoBox/routes/index.js
+++ MycoBox/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 // The Controller Classes need to be instantiated before getting here...
-const { newSession } = require('../controllers/system.controller');
+const { newSession } = require('../controllers/system.controller/system.controller');
 /**
  * Route HTTP requests to correct controllers
  */
