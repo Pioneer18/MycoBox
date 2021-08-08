e8a1364d8d7a802b397f15403f2eda8863448e2f
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-08 15:48:46 -0400

    getting ready to pass real sensor data to the Frontend

--- FrontEnd/public/javascript/controllers/dashboard/dashboard.js
+++ FrontEnd/public/javascript/controllers/dashboard/dashboard.js
@@ -3,5 +3,21 @@
  */
 console.log('Starting the Dashboard Controller')
 
-document.getElementById('temp_field').innerHTML = '35C';
+const insert_sensor_values = (values) => {
+    for (val in values) {
+        switch (val)) {
+            case 'temp':
+                document.getElementById('temp_field').innerHTML = '35C';
+                break;
+            case 'humidity':
+                document.getElementById('humidity_field').innerHTML = '35C';
+            case 'co2':
+                document.getElementById('co2_field').innerHTML = '35C';
+            default:
+                break;
+        }
+    }
+}
+const values = ['temp', 'humidity','co2']
+insert_sensor_values(values)
     
