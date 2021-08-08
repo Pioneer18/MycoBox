746f7fb75906609c8ada1a97714151f9b46fd921
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-08 15:43:24 -0400

    simple test

--- FrontEnd/public/javascript/controllers/dashboard/dashboard.js
+++ FrontEnd/public/javascript/controllers/dashboard/dashboard.js
@@ -2,3 +2,6 @@
  * Dashboard Controller
  */
 console.log('Starting the Dashboard Controller')
+
+document.getElementById('temp_field').innerHTML = '35C';
+    
--- FrontEnd/public/javascript/controllers/dashboard/index.js
+++ FrontEnd/public/javascript/controllers/dashboard/index.js
@@ -2,5 +2,5 @@
  * This is the file linked to the html, it's going to server up the DashboardController
  */
 import DashboardController from './dashboard';
-console.log('Starting the Dashboard Controller')
+
 DashboardController();
