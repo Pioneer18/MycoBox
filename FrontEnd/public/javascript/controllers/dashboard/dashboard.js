fcb5a31aacfaa8d70a7e9fa0da343e877a3a39a6
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-08 22:41:10 -0400

    big test

--- FrontEnd/public/javascript/controllers/dashboard/dashboard.js
+++ FrontEnd/public/javascript/controllers/dashboard/dashboard.js
@@ -2,7 +2,7 @@
  * Dashboard Controller
  */
 console.log('Starting the Dashboard Controller')
-import {FrontEndApi} from '../../api/index.js';
+import { FrontEndApi } from '../../api/index.js';
 const api = new FrontEndApi();
 
 const insert_sensor_values = (values) => {
@@ -11,11 +11,22 @@ const insert_sensor_values = (values) => {
     console.log(`Values from DashboardController`)
     console.log(values)
     for (const val in values) {
-        if(val === 'temp') document.getElementById('iTemp1').innerHTML = values[val];
-        if(val === 'humidity') document.getElementById('iTemp2').innerHTML = values[val];
-        if(val === 'co2') document.getElementById('iTemp3').innerHTML = values[val];
+        if (val === 'temp') document.getElementById('iTemp1').innerHTML = values[val]
+        if (val === 'humidity') document.getElementById('iTemp2').innerHTML = values[val]
+        if (val === 'co2') document.getElementById('iTemp3').innerHTML = values[val]
+        if (val == 'iTemp1') document.getElementById('iTemp1').innerHTML = values[val]
+        if (val == 'iTemp2') document.getElementById('iTemp2').innerHTML = values[val]
+        if (val == 'iTemp3') document.getElementById('iTemp3').innerHTML = values[val]
+        if (val == 'pTempC') document.getElementById('pTempC').innerHTML = values[val]
+        if (val == 'pTempF') document.getElementById('pTempF').innerHTML = values[val]
+        if (val == 'eTemp') document.getElementById('eTemp').innerHTML = values[val]
+        if (val == 'iHumidity1') document.getElementById('iHumidity1').innerHTML = values[val]
+        if (val == 'iHumidity2') document.getElementById('iHumidity2').innerHTML = values[val]
+        if (val == 'iHumidity3') document.getElementById('iHumidity3').innerHTML = values[val]
+        if (val == 'eHumidity') document.getElementById('eHumidity').innerHTML = values[val]
+        if (val == 'co2') document.getElementById('co2').innerHTML = values[val]
+        if (val == 'weight') document.getElementById('weight').innerHTML = values[val]
     }
 }
 
 insert_sensor_values(values)
-    
