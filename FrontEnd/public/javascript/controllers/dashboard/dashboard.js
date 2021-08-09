dd2673cdac25d5cc4ab8b2241c70c4945bd1f078
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-08 22:22:50 -0400

    trying to fix broken ui

--- FrontEnd/public/javascript/controllers/dashboard/dashboard.js
+++ FrontEnd/public/javascript/controllers/dashboard/dashboard.js
@@ -11,11 +11,11 @@ const insert_sensor_values = (values) => {
     console.log(`Values from DashboardController`)
     console.log(values)
     for (const val in values) {
-        if(val === 'temp') document.getElementById('temp_field').innerHTML = values[val];
-        if(val === 'humidity') document.getElementById('humidity_field').innerHTML = values[val];
-        if(val === 'co2') document.getElementById('co2_field').innerHTML = values[val];
+        if(val === 'temp') document.getElementById('iTemp1').innerHTML = values[val];
+        if(val === 'humidity') document.getElementById('iTemp2').innerHTML = values[val];
+        if(val === 'co2') document.getElementById('iTemp3').innerHTML = values[val];
     }
 }
-const values = {'temp': 35, 'humidity': 99,'co2': 15000}
+
 insert_sensor_values(values)
     
