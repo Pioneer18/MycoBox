67ebb330d15e3d33338feadadbcfd7754400f1cf
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-10 23:10:13 -0400

    it should be looking for api/dashboard.js not index.js

--- FrontEnd/public/javascript/controllers/dashboard/dashboard.js
+++ FrontEnd/public/javascript/controllers/dashboard/dashboard.js
@@ -1,8 +1,8 @@
 /**
  * Dashboard Controller
  */
+import {readEnvironmentModel} from '../../api/dashboard.js';
 console.log('Starting the Dashboard Controller')
-import { readEnvironmentModel } from '../../api/dashboard.js';
 
 const insert_sensor_values = () => {
     console.log('Reading the Environment Model')
