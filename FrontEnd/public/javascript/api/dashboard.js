0b33cadc5b3db53e0c74b4d9eee239887c478393
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-10 23:26:59 -0400

    no importing api/index.js at all

--- FrontEnd/public/javascript/api/dashboard.js
+++ FrontEnd/public/javascript/api/dashboard.js
@@ -1,4 +1,3 @@
-import {makeRequest} from './index'
 const api = 'api_v1'
 
 /**
@@ -11,4 +10,42 @@ const api = 'api_v1'
 
 export {
     readEnvironmentModel
+}
+
+const makeRequest = (method, url, data) => {
+    // create the XMLHTTP instance for creating Ajax call
+    let httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+        alert('Error: Cannot create an XMLHTTP instance');
+        return false;
+    }
+    // Handle the HTTP Response
+    httpRequest.onreadystatechange = () => {
+        // process the server response here
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            // Everything is good, the response was received.
+            if (httpRequest.status === 200) {
+                // Perfect!
+                console.log('Below is the backend http response:')
+                // console.log(httpRequest.responseText)
+                console.log(httpRequest.response)
+                console.log(JSON.parse(httpRequest.response))
+            } else {
+                // There was a problem with the request.
+                // For example, the response may have a 404 (Not Found)
+                // or 500 (Internal Server Error) response code.
+                console.log('Some Kinda Error??')
+                return
+            }
+        } else {
+            // Not ready yet.
+            console.log('Not Ready yet?')
+            return
+        }
+    }
+    httpRequest.open(method, url, true); // async true
+    httpRequest.setRequestHeader('Access-Control-Allow-Headers', '*');
+    httpRequest.setRequestHeader('Access-Control-Allow-Origin', 'http://192.168.1.97:3000/');
+    httpRequest.setRequestHeader('Content-Type', 'application/json');
+    httpRequest.send(data);
 }
