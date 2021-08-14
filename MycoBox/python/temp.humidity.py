0f3fd5d97f42f7ec0206c89a6fdc5611f8e903f4
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-14 18:28:55 -0400

    created async python script

--- MycoBox/python/temp.humidity.py
+++ MycoBox/python/temp.humidity.py
@@ -5,20 +5,27 @@ DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_1 = 24  # bottom dht22
 DHT_2 = 25  # mid dht22
 DHT_3 = 1  # top dht22
-DHT_4 = 12 # external dht22
-sensor_list = [1,2,3]
+DHT_4 = 12  # external dht22
+sensor_list = [1, 2, 3]
 h1 = 0
 h2 = 0
 h3 = 0
+h4 = 0
 temp1 = 0
 temp2 = 0
 temp3 = 0
 temp4 = 0
 
-h1, temp1 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_1)
-h2, temp2 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_2)
-h3, temp3 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_3)
-h4, temp4 = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_4)
+async def getReadings():
+    try:
+        h1, temp1 = await Adafruit_DHT.read_retry(DHT_SENSOR, DHT_1)
+        h2, temp2 = await Adafruit_DHT.read_retry(DHT_SENSOR, DHT_2)
+        h3, temp3 = await Adafruit_DHT.read_retry(DHT_SENSOR, DHT_3)
+        h4, temp4 = await Adafruit_DHT.read_retry(DHT_SENSOR, DHT_4)
+        print('h1={', h1, '} h2={', h2, '} h3={', h3, '} h4={', h4, '} temp1={',
+              temp1, '} temp2={', temp2, '} temp3={', temp3, '} temp4={', temp4, '}')
 
-print('h1={', h1, '} h2={', h2, '} h3={', h3, '} h4={', h4, '} temp1={',
-      temp1, '} temp2={', temp2, '} temp3={', temp3, '} temp4={', temp4, '}')
+    except ValueError:
+        print('Exception: ValueError Getting Temp & Humidity Readings')
+
+getReadings()
