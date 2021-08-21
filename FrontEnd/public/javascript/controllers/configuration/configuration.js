3fafc3e54333754bc47c967262bc1b074769d091
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-20 22:08:43 -0400

    debugging

--- FrontEnd/public/javascript/controllers/configuration/configuration.js
+++ FrontEnd/public/javascript/controllers/configuration/configuration.js
@@ -8,15 +8,15 @@ let DEFAULTS = true;
 
 const displayOverrides = () => {
     let rad = document.main_config_form.accept_defaults;
-    const overrides = document.getElementById('custom_config');
+    const custom_config = document.getElementById('custom_config');
     for (let i = 0; i < rad.length; i++) {
         rad[i].addEventListener('change', () => {
             if (rad[i].value == 'Yes') {
-                overrides.classList.add('hidden');
+                custom_config.classList.add('hidden');
                 DEFAULTS = true;
             }
             if (rad[i].value !== 'Yes') {
-                overrides.classList.remove('hidden');
+                custom_config.classList.remove('hidden');
                 DEFAULTS = false
             }
         });
