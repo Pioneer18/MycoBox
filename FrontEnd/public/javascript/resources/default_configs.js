3af3d6fcc6661fa3cac9cdec418cbd618035323b
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-20 20:41:25 -0400

    updated the default config format

--- FrontEnd/public/javascript/resources/default_configs.js
+++ FrontEnd/public/javascript/resources/default_configs.js
@@ -1,63 +1,135 @@
 
 const default_configs = {
-    incubation: {
-        pink_oyster: {
-            temperature: '',
-            humidity: '',
-            co2: '',
-            lighting: false,
-            duration: 35 // days
+    pink_oyster: {
+        spawn_running: {
+            ir_temp_trigger: true,
+            timer: number || null,
+            temp_setpoint: '',
+            irTemp_setpoint: '', // trigger session end
+            humidity_setpoint: '',
+            co2_setpoint: '',
+            circulation_top: '',
+            circulation_bottom: '',
+            lighting_setpoint: ''
         },
-        golden_oyster: {
-            temperature: '',
-            humidity: '',
-            co2: '',
-            lighting: false,
-            duration: 35 // days
+        primordia_init: {
+            user_confirmed: false,
+            timer: number || null,
+            temp_setpoint: '',
+            humidity_setpoint: '',
+            co2_setpoint: '',
+            circulation_top: '',
+            circulation_bottom: '',
+            lighting_setpoint: ''
         },
-        blue_oyster: {
-            temperature: '',
-            humidity: '',
-            co2: '',
-            lighting: false,
-            duration: 35 // days
-        },
-        shitake: {
-            temperature: '',
-            humidity: '',
-            co2: '',
-            lighting: false,
-            duration: 35 // days
+        fruiting: {
+            user_confirmed: false,
+            timer: number || null,
+            temp_setpoint: '',
+            humidity_setpoint: '',
+            co2_setpoint: '',
+            circulation_top: '',
+            circulation_bottom: '',
+            lighting_setpoint: ''
         }
     },
-    fruiting: {
-        pink_oyster: {
-            temperature: '', // celsius
-            humidity: '',
-            co2: '', // ppm
-            lighting: true,
-            duration: 35 // days
+    golden_oyster: {
+        spawn_running: {
+            ir_temp_trigger: true,
+            timer: number || null,
+            temp_setpoint: '',
+            irTemp_setpoint: '', // trigger session end
+            humidity_setpoint: '',
+            co2_setpoint: '',
+            circulation_top: '',
+            circulation_bottom: '',
+            lighting_setpoint: ''
         },
-        golden_oyster: {
-            temperature: '', // celsius
-            humidity: '',
-            co2: '', // ppm
-            lighting: true,
-            duration: 35 // days
+        primordia_init: {
+            user_confirmed: false,
+            timer: number || null,
+            temp_setpoint: '',
+            humidity_setpoint: '',
+            co2_setpoint: '',
+            circulation_top: '',
+            circulation_bottom: '',
+            lighting_setpoint: ''
         },
-        blue_oyster: {
-            temperature: '', // celsius
-            humidity: '',
-            co2: '', // ppm
-            lighting: true,
-            duration: 35 // days
+        fruiting: {
+            user_confirmed: false,
+            timer: number || null,
+            temp_setpoint: '',
+            humidity_setpoint: '',
+            co2_setpoint: '',
+            circulation_top: '',
+            circulation_bottom: '',
+            lighting_setpoint: ''
+        }
+    },
+    blue_oyster: {
+        spawn_running: {
+            ir_temp_trigger: true,
+            timer: number || null,
+            temp_setpoint: '',
+            irTemp_setpoint: '', // trigger session end
+            humidity_setpoint: '',
+            co2_setpoint: '',
+            circulation_top: '',
+            circulation_bottom: '',
+            lighting_setpoint: ''
         },
-        shitake: {
-            temperature: '', // celsius
-            humidity: '',
-            co2: '', // ppm
-            lighting: true,
-            duration: 35 // days
+        primordia_init: {
+            user_confirmed: false,
+            timer: number || null,
+            temp_setpoint: '',
+            humidity_setpoint: '',
+            co2_setpoint: '',
+            circulation_top: '',
+            circulation_bottom: '',
+            lighting_setpoint: ''
+        },
+        fruiting: {
+            user_confirmed: false,
+            timer: number || null,
+            temp_setpoint: '',
+            humidity_setpoint: '',
+            co2_setpoint: '',
+            circulation_top: '',
+            circulation_bottom: '',
+            lighting_setpoint: ''
+        }
+    },
+    shitake: {
+        spawn_running: {
+            ir_temp_trigger: true,
+            timer: number || null,
+            temp_setpoint: '',
+            irTemp_setpoint: '', // trigger session end
+            humidity_setpoint: '',
+            co2_setpoint: '',
+            circulation_top: '',
+            circulation_bottom: '',
+            lighting_setpoint: ''
+        },
+        primordia_init: {
+            user_confirmed: false,
+            timer: number || null,
+            temp_setpoint: '',
+            humidity_setpoint: '',
+            co2_setpoint: '',
+            circulation_top: '',
+            circulation_bottom: '',
+            lighting_setpoint: ''
+        },
+        fruiting: {
+            user_confirmed: false,
+            timer: number || null,
+            temp_setpoint: '',
+            humidity_setpoint: '',
+            co2_setpoint: '',
+            circulation_top: '',
+            circulation_bottom: '',
+            lighting_setpoint: ''
         }
     }
 }
