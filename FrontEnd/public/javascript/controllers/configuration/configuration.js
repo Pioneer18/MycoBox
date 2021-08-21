ec26a2a08de354b9386c3485b2a1ac6d22660205
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-20 21:04:11 -0400

    updating the form and mapping function

--- FrontEnd/public/javascript/controllers/configuration/configuration.js
+++ FrontEnd/public/javascript/controllers/configuration/configuration.js
@@ -27,15 +27,14 @@ const displayOverrides = () => {
 document.main_config_form.onsubmit = (e) => {
     e.preventDefault();
     // map the form
-    const { process, config } = mapForm();
+    const {config } = mapForm();
     // send Http request to start process on MycoBox
-    newSession(process, config)
+    newSession(config)
 };
 
 const mapForm = () => {
     let form = document.main_config_form;
     let mushroom = form.mushroom.value;
-    let process = form.process.value;
     // Start process with overrrides
     if (!DEFAULTS) {
         let overrides = {};
@@ -44,9 +43,9 @@ const mapForm = () => {
         overrides.co2_ppm = form.co2_ppm.value;
         overrides.lighting = form.lighting.value;
         overrides.duration_days = form.duration_days.value;
-        return { process, overrides };
+        return { overrides };
     }
-    return { process, config: default_configs[process][mushroom] }
+    return { config: default_configs[mushroom] }
 
 }
 
