e40763d04a627217aaf371c0a804aa14de818252
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-22 14:02:50 -0400

    testing the form mapping

--- MycoBox/routes/configuration.routes.js
+++ MycoBox/routes/configuration.routes.js
@@ -11,7 +11,7 @@ const { newSession } = require('../controllers/system.controller/system.controll
 router.post(`/system_controller/start_session`, async (req, res) => {
     try {
         console.log("here is the submitted config object:")
-        console.log(req)
+        console.log(req.body)
        res.send(JSON.stringify(newSession('tee hee')))
     } catch (err) {
         console.log(`Error: ${err}`)
