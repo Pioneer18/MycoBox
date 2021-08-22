9f1f446f61bbb673d2eae5db982abfcf6a4d1eb4
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-22 10:24:45 -0400

    testing eventListener on the sr_timer

--- FrontEnd/public/javascript/controllers/configuration/configuration.js
+++ FrontEnd/public/javascript/controllers/configuration/configuration.js
@@ -70,9 +70,8 @@ displayOverrides();
 //spawnRunningTrigger();
 // Dynamic UI EventListeners
 
-// const sr_timer = document.getElementById('sr-timer')
-// sr_timer.addEventListener('change', () => {
-//     const button = document.getElementById('sr-timer')
-//     if (button === true) console.log('Use A Timer!') 
-//     if (button.checked === false) console.log('Use the IR sensor!!') 
-// })
+const sr_timer = document.getElementById('sr_timer')
+sr_timer.addEventListener('change', () => {
+    if (sr_timer === true) console.log('Use A Timer!') 
+    if (sr_timer.checked === false) console.log('Use the IR sensor!!') 
+})
