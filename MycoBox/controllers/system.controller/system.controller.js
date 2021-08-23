630927f46b569cf2b12eff2230c4e8e1a3563f51
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-22 20:25:51 -0400

    test

--- MycoBox/controllers/system.controller/system.controller.js
+++ MycoBox/controllers/system.controller/system.controller.js
@@ -41,7 +41,7 @@ const { set_environment_state } = require("../sensors.controller/sensors.control
                 // console.log(`Starting session ${globals.session_state.session_title} - ${globals.session_state.session_id}`);
                 await set_environment_config(config);
                 await set_environment_state();
-                console.log(get('environment_stat'))
+                console.log(get('environment_state'))
                 // await environment_manager();
             } else {
                 throw new Error('There is already an active session');
