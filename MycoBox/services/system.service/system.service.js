667ecc8b9939cbd5b8413740345735b44ff54c20
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-22 20:56:36 -0400

    test

--- MycoBox/services/system.service/system.service.js
+++ MycoBox/services/system.service/system.service.js
@@ -14,7 +14,7 @@ const { get } = require("../../globals/globals")
  */
 const environment_manager = async () => {
     const env_config = await get('environment_config');
-    const env_state = await get ('environment_state');
+    const env_state = await get ('environment_state'); 
     console.log('env_config from within environment manager: ' + env_config)
     console.log('env_state from within environment manager: ' + env_state)
 }
