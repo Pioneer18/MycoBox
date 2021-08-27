4eda18c50f639c7efb5228b8da72a429f922c495
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-27 17:43:00 -0400

    deleted an unused directory

--- backend/javascript/subroutines/readCO2.js
+++
@@ -1,6 +0,0 @@
-const { PythonShell } = require("python-shell");
-
-PythonShell.run('../../../python/CO2.py', null, function(err, results) {
-    if (err) throw err;
-    console.log('Results: %j', results);
-})
