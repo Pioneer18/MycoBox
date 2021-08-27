21663b53aad4a4507d22cda4e5d90e84448113a5
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-27 15:42:21 -0400

    test

--- MycoBox/controllers/sensors.controller/sensors.controller.js
+++ MycoBox/controllers/sensors.controller/sensors.controller.js
@@ -66,6 +66,8 @@ const read_infrared = () => {
 }
 
 const set_timestamp = async () => {
+    console.log('Setting the timestamp');
+    console.log(typeof Date.now)
     set_environment_state('timestamp', Date.now())
 }
 
