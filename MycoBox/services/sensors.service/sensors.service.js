482961572b9b964cfc4dfb0fe9a0a35f2a79fa4e
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-28 14:50:54 -0400

    updated the validate_th_data promise to use resolve/reject

--- MycoBox/services/sensors.service/sensors.service.js
+++ MycoBox/services/sensors.service/sensors.service.js
@@ -1,51 +1,52 @@
 /**
  * Sensors Service
  */
-const {set_environment_state} = require('../../globals/globals');
+const { set_environment_state } = require('../../globals/globals');
 
 /**
  * Parse incoming dht22 sensor data
  * @param {Array} reply h1,h2,h3,h4,temp1,temp2,
  * @returns 
  */
-const parse_th_data = async (reply) => {
+const parse_th_data = new Promise((resolve, reject) => {
     const data = JSON.stringify(reply[0].match(/[^{}]+(?=\})/g)).split('"')
-    for(let i = 1; i < 16; i +=2) {
-        await validate_th_data(data[i]) // probably should skip if not valid...
-        await set_dht22_values(i, data[i])
+    for (let i = 1; i < 16; i += 2) {
+        validate_th_data(data[i]) // probably should skip if not valid...
+        set_dht22_values(i, data[i])
     }
     return
-}
+})
 
-const parse_pt_data = async (reply) => {
+const parse_pt_data = new Promise((resolve, reject) => {
     const data = JSON.stringify(reply[0].match(/[^{}]+(?=\})/g)).split('"')
-    await set_environment_state('precise_temp_c', data[1]);
-    await set_environment_state('precise_temp_f', data[3]);
+    set_environment_state('precise_temp_c', data[1]);
+    set_environment_state('precise_temp_f', data[3]);
     return
-}
+})
 
-const parse_co2_data = async (reply) => {
+const parse_co2_data = new Promise((resolve, reject) => {
     const data = JSON.stringify(reply[0].match(/[^{}]+(?=\})/g)).split('"')
     console.log('CO2 Data Below !!!!!!!!!')
     console.log(data)
-    await set_environment_state('co2', data[0]);
+    set_environment_state('co2', data[0]);
     return
-}
+})
 
 /**
  * Validate parsed sensor data
  * @param {Array} data e.g. ['43.55', 44.25, 43.40]
  */
-const validate_th_data = async (data) => {
-    if (typeof data !== typeof 'string') console.error(`received temp/humidity value that is not a string, check the sensors! ${data}`)
-    if (data === '') console.error(`received an empty value for temp/humidity, check the sensors! ${data}`)
-    if (data == null || data == undefined) console.error(`uh oh, got a null or undefined sensor value, check the sensors! ${data}`)
-}
+const validate_th_data = new Promise((resolve, reject) => {
+    if (typeof data !== typeof 'string') reject(`received temp/humidity value that is not a string, check the sensors! ${data}`)
+    if (data === '') reject(`received an empty value for temp/humidity, check the sensors! ${data}`)
+    if (data == null || data == undefined) reject(`uh oh, got a null or undefined sensor value, check the sensors! ${data}`)
+    resolve()
+})
 
 /**
  * Set dht22 values to environment model
  */
-const set_dht22_values = async (i, data) => {
+const set_dht22_values = new Promise((resolve, reject) => {
     switch (i) {
         case 1:
             await set_environment_state('internal_humidity_1', data);
@@ -74,7 +75,7 @@ const set_dht22_values = async (i, data) => {
         default:
             break;
     }
-}
+})
 
 module.exports = {
     parse_th_data,
