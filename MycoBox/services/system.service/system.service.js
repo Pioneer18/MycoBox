8ac884489dd2b3821e33dc28a1b58cf7831a18e0
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-28 19:14:21 -0400

    debug

--- MycoBox/services/system.service/system.service.js
+++ MycoBox/services/system.service/system.service.js
@@ -85,21 +85,13 @@ const validate_env_state = () => {
                 if (env_state.timestamp === 'Initial') {
                     console.log('Validate Env Recall: Timpestamp === Initial')
                     initialize_environment_state()
-                        .then(() => {
-                            setTimeout(() => {
-                                validate_env_state()
-                            }, 8000);
-                        })
+                        .then(() => validate_env_state())
                     // wait, and check again
                 }
                 if (env_state.internal_temp_1 === '') {
                     console.log('Validate Env Recall: Blank Env State')
                     initialize_environment_state()
-                        .then(() => {
-                            setTimeout(() => {
-                                validate_env_state()
-                            }, 8000);
-                        })
+                        .then(() => validate_env_state())
                 }
                 if (env_state.internal_temp_1 !== '' && env_state.external_humidity !== '') {
                     console.log('Valid Env State!')
