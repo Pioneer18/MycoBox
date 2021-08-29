b501aca337477acc9293b1a0ced82bdce7d60ec5
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-29 17:10:05 -0400

    test

--- FrontEnd/public/javascript/controllers/dashboard/dashboard.js
+++ FrontEnd/public/javascript/controllers/dashboard/dashboard.js
@@ -13,18 +13,18 @@ const insert_sensor_values = async () => {
     const values = await readEnvironmentModel()
     console.log('Here are the Values from within the controller')
     console.log(values)
-    document.getElementById('iTemp1').innerHTML = values['internal_temp_1'].toFixed(2)
-    document.getElementById('iTemp2').innerHTML = values['internal_temp_2'].toFixed(2)
-    document.getElementById('iTemp3').innerHTML = values['internal_temp_3'].toFixed(2)
-    document.getElementById('pTempC').innerHTML = values['precise_temp_c'].toFixed(2)
-    document.getElementById('pTempF').innerHTML = values['precise_temp_f'].toFixed(2)
-    document.getElementById('eTemp').innerHTML = values['external_temp'].toFixed(2)
-    document.getElementById('iHumidity1').innerHTML = values['internal_humidity_1'].toFixed(2)
-    document.getElementById('iHumidity2').innerHTML = values['internal_humidity_2'].toFixed(2)
-    document.getElementById('iHumidity3').innerHTML = values['internal_humidity_3'].toFixed(2)
-    document.getElementById('eHumidity').innerHTML = values['external_humidity'].toFixed(2)
-    document.getElementById('co2').innerHTML = values['co2'].toFixed(2)
-    document.getElementById('weight').innerHTML = values['weight'].toFixed(2)
+    document.getElementById('iTemp1').innerHTML = (values['internal_temp_1']).toFixed(2)
+    document.getElementById('iTemp2').innerHTML = values['internal_temp_2']
+    document.getElementById('iTemp3').innerHTML = values['internal_temp_3']
+    document.getElementById('pTempC').innerHTML = values['precise_temp_c']
+    document.getElementById('pTempF').innerHTML = values['precise_temp_f']
+    document.getElementById('eTemp').innerHTML = values['external_temp']
+    document.getElementById('iHumidity1').innerHTML = values['internal_humidity_1']
+    document.getElementById('iHumidity2').innerHTML = values['internal_humidity_2']
+    document.getElementById('iHumidity3').innerHTML = values['internal_humidity_3']
+    document.getElementById('eHumidity').innerHTML = values['external_humidity']
+    document.getElementById('co2').innerHTML = values['co2']
+    document.getElementById('weight').innerHTML = values['weight']
 }
 
 function myFunction() {
