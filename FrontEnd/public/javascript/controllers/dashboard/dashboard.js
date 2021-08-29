24f13091cabe59e60f2be1b4d2fa3917afc09d0f
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-29 18:11:09 -0400

    cleaned sensor values

--- FrontEnd/public/javascript/controllers/dashboard/dashboard.js
+++ FrontEnd/public/javascript/controllers/dashboard/dashboard.js
@@ -13,16 +13,16 @@ const insert_sensor_values = async () => {
     const values = await readEnvironmentModel()
     console.log('Here is internal_temp_1')
     console.log(parseFloat(values['internal_temp_1']))
-    document.getElementById('iTemp1').innerHTML = (values['internal_temp_1'])
-    document.getElementById('iTemp2').innerHTML = values['internal_temp_2']
-    document.getElementById('iTemp3').innerHTML = values['internal_temp_3']
-    document.getElementById('pTempC').innerHTML = values['precise_temp_c']
-    document.getElementById('pTempF').innerHTML = values['precise_temp_f']
-    document.getElementById('eTemp').innerHTML = values['external_temp']
-    document.getElementById('iHumidity1').innerHTML = values['internal_humidity_1']
-    document.getElementById('iHumidity2').innerHTML = values['internal_humidity_2']
-    document.getElementById('iHumidity3').innerHTML = values['internal_humidity_3']
-    document.getElementById('eHumidity').innerHTML = values['external_humidity']
+    document.getElementById('iTemp1').innerHTML = parseFloat(values['internal_temp_1']).toFixed(2)
+    document.getElementById('iTemp2').innerHTML = parseFloat(values['internal_temp_2']).toFixed(2)
+    document.getElementById('iTemp3').innerHTML = parseFloat(values['internal_temp_3']).toFixed(2)
+    document.getElementById('pTempC').innerHTML = parseFloat(values['precise_temp_c']).toFixed(2)
+    document.getElementById('pTempF').innerHTML = parseFloat(values['precise_temp_f']).toFixed(2)
+    document.getElementById('eTemp').innerHTML = parseFloat(values['external_temp']).toFixed(2)
+    document.getElementById('iHumidity1').innerHTML = parseFloat(values['internal_humidity_1']).toFixed(2)
+    document.getElementById('iHumidity2').innerHTML = parseFloat(values['internal_humidity_2']).toFixed(2)
+    document.getElementById('iHumidity3').innerHTML = parseFloat(values['internal_humidity_3']).toFixed(2)
+    document.getElementById('eHumidity').innerHTML = parseFloat(values['external_humidity']).toFixed(2)
     document.getElementById('co2').innerHTML = values['co2']
     document.getElementById('weight').innerHTML = values['weight']
 }
