c32dd665397860ccaa51029c6f99c0e7c5d822ea
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-29 17:57:06 -0400

    debug

--- FrontEnd/public/javascript/controllers/dashboard/dashboard.js
+++ FrontEnd/public/javascript/controllers/dashboard/dashboard.js
@@ -13,7 +13,7 @@ const insert_sensor_values = async () => {
     const values = await readEnvironmentModel()
     console.log('Here are the Values from within the controller')
     console.log(values)
-    document.getElementById('iTemp1').innerHTML = (values['internal_temp_1']).toFixed(2)
+    document.getElementById('iTemp1').innerHTML = (values['internal_temp_1'])
     document.getElementById('iTemp2').innerHTML = values['internal_temp_2']
     document.getElementById('iTemp3').innerHTML = values['internal_temp_3']
     document.getElementById('pTempC').innerHTML = values['precise_temp_c']
