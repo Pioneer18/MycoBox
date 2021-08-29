6f914c748ccbef3f372a2fc65ec687d7a7fea049
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-29 16:50:12 -0400

    setInterval to reset dashboard every 9 seconds

--- FrontEnd/public/javascript/controllers/dashboard/dashboard.js
+++ FrontEnd/public/javascript/controllers/dashboard/dashboard.js
@@ -9,33 +9,25 @@ console.log('Starting the Dashboard Controller')
 const active_session = true;
 
 const insert_sensor_values = async () => {
-    let cleared = true;
-    while (active_session) {
-        if (cleared) {
-            cleared = false;
-            console.log('Reading the Environment Model')
-            const values = await readEnvironmentModel()
-            console.log('Here are the Values from within the controller')
-            console.log(values)
-            document.getElementById('iTemp1').innerHTML = values['internal_temp_1']
-            document.getElementById('iTemp2').innerHTML = values['internal_temp_2']
-            document.getElementById('iTemp3').innerHTML = values['internal_temp_3']
-            document.getElementById('pTempC').innerHTML = values['precise_temp_c']
-            document.getElementById('pTempF').innerHTML = values['precise_temp_f']
-            document.getElementById('eTemp').innerHTML = values['external_temp']
-            document.getElementById('iHumidity1').innerHTML = values['internal_humidity_1']
-            document.getElementById('iHumidity2').innerHTML = values['internal_humidity_2']
-            document.getElementById('iHumidity3').innerHTML = values['internal_humidity_3']
-            document.getElementById('eHumidity').innerHTML = values['external_humidity']
-            document.getElementById('co2').innerHTML = values['co2']
-            document.getElementById('weight').innerHTML = values['weight']
-            setTimeout(() => {
-                // refresh the dashboard every 5 seconds
-                cleared = true;
-            }, 5000);
-        }
-    }
+    console.log('Reading the Environment Model')
+    const values = await readEnvironmentModel()
+    console.log('Here are the Values from within the controller')
+    console.log(values)
+    document.getElementById('iTemp1').innerHTML = values['internal_temp_1']
+    document.getElementById('iTemp2').innerHTML = values['internal_temp_2']
+    document.getElementById('iTemp3').innerHTML = values['internal_temp_3']
+    document.getElementById('pTempC').innerHTML = values['precise_temp_c']
+    document.getElementById('pTempF').innerHTML = values['precise_temp_f']
+    document.getElementById('eTemp').innerHTML = values['external_temp']
+    document.getElementById('iHumidity1').innerHTML = values['internal_humidity_1']
+    document.getElementById('iHumidity2').innerHTML = values['internal_humidity_2']
+    document.getElementById('iHumidity3').innerHTML = values['internal_humidity_3']
+    document.getElementById('eHumidity').innerHTML = values['external_humidity']
+    document.getElementById('co2').innerHTML = values['co2']
+    document.getElementById('weight').innerHTML = values['weight']
 }
 
-
-insert_sensor_values()
+function myFunction() {
+    setInterval(function () { insert_sensor_values(); }, 9000);
+}
+myFunction()
