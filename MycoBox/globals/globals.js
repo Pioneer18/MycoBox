dba754f8a1c6acfae1eaea8454a3eac0d830e033
Jonathan Sells <pioneer@jonathans-air.home>
2021-08-31 22:48:00 -0400

    debugging

--- MycoBox/globals/globals.js
+++ MycoBox/globals/globals.js
@@ -344,9 +344,9 @@ const set_actuator_state = (element, status, value) => {
 const validate_set_actuator_state = (element, status, value) => {
     if (!element || (typeof element !== 'string')) throw new Error('Missing or Invalid Element for setting actuator state');
     if (!value || !((typeof value === 'boolean') || (typeof value === 'number'))) throw new Error('Missing or Invalid Value for setting actuator state');
-    if (element === 'mb_light_1' || element === 'mb_light_2' || element === 'ib_light' || element === 'speakers') {
-        return
-    }
+    // if (element === 'mb_light_1' || element === 'mb_light_2' || element === 'ib_light' || element === 'speakers') {
+    //     return
+    // }
     if (!status || (typeof status !== 'string')) throw new Error('Missing or Invalid status given for setting actuator state');
     return
 }
