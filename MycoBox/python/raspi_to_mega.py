0e0557da707fe043870e78c244e777797e9ee8fd
Jonathan Sells <pioneer@jonathans-air.home>
2021-09-12 17:05:14 -0400

    added python script to read serial data from arduino

---
+++ MycoBox/python/raspi_to_mega.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import serial
+
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+ser.flush()
+while True:
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
