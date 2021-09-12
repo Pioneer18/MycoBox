4847cac4e3f1918a8bbb70732e85d3797b71c2a7
Jonathan Sells <pioneer@jonathans-air.home>
2021-09-12 19:44:03 -0400

    testing serial comms to mega

--- MycoBox/python/raspi_to_mega.py
+++ MycoBox/python/raspi_to_mega.py
@@ -11,6 +11,6 @@ import time
 ser = serial.Serial('/dev/ttyACM0', 115200)
 while True:
     ser.write("H 25\r\n".encode())
-    time.sleep(1)
+    time.sleep(2)
     line = ser.readline().decode('utf-8').rstrip()
     print(line)
