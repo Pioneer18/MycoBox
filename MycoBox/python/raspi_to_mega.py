30288faa93d1ab67c231e146027466f457bfbc0d
Jonathan Sells <pioneer@jonathans-air.home>
2021-09-13 14:24:37 -0400

    testing serial comms, new approach

--- MycoBox/python/raspi_to_mega.py
+++ MycoBox/python/raspi_to_mega.py
@@ -2,14 +2,13 @@
 import serial
 import time
 
-# ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-# ser.flush()
-# while True:
-#     if ser.in_waiting > 0:
-#         line = ser.readline().decode('utf-8').rstrip()
-#         print(line)
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+ser.flush()
 while True:
-    ser.write("H 25\r\n".encode())
-    line = ser.readline().decode('utf-8').rstrip()
-    print(line)
+    print('Sending Command: H 25')
+    ser.write("H 25\n".encode())
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+    time.sleep(1)
+    
