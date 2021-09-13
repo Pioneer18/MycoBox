21593d09df7d8b0a64cd92870bd82275516265e8
Jonathan Sells <pioneer@jonathans-air.home>
2021-09-13 18:20:15 -0400

    tyring serial.readline instead of read(10)

--- MycoBox/python/raspi_to_mega.py
+++ MycoBox/python/raspi_to_mega.py
@@ -5,10 +5,9 @@ import time
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 ser.flush()
 while True:
-    print('Sending Command: H 25')
     ser.write("H 25\n".encode())
     if ser.in_waiting > 0:
-        line = ser.read(10).decode('utf-8')
+        line = ser.readline().decode('utf-8')
         print(line)
     time.sleep(1)
 # ser.readline().decode('utf-8').rstrip()
