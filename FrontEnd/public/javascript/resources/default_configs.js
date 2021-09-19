ead56ffc95ccdbde708a02b52eb1337aec9a7053
Jonathan Sells <pioneer@jonathans-air.home>
2021-09-19 12:21:20 -0400

    changed temp config

--- FrontEnd/public/javascript/resources/default_configs.js
+++ FrontEnd/public/javascript/resources/default_configs.js
@@ -2,7 +2,7 @@
 const default_configs = {
     pink_oyster: { 
         spawn_running: {
-            temperature: '31',
+            temperature: '25',
             humidity: '75',
             co2: '15000',
             circulation_top: 'off',
