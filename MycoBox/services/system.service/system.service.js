611d65ae6e9f85ea544ed8274e6d503f32ef8e7c
Jonathan Sells <pioneer@jonathans-air.home>
2021-09-19 16:55:26 -0400

    rollback and update

--- MycoBox/services/system.service/system.service.js
+++ MycoBox/services/system.service/system.service.js
@@ -130,7 +130,7 @@ const validate_env_state = () => {
                     //             recheck_env_state(resolve)
                     //         }, 8000);
                     //     })
-                    await recheck_env_state(resolve)
+                    recheck_env_state(resolve)
                 }
                 if (typeof env_state.internal_temp_1 === 'number' ||
                     typeof env_state.internal_temp_2 === 'number' ||
@@ -149,8 +149,8 @@ const validate_env_state = () => {
     })
 }
 
-const recheck_env_state = async (resolve) => {
-    try{read_environment_state()
+const recheck_env_state = (resolve) => {
+    read_environment_state()
         .then(env_state => {
             console.log("Validation Recheck: ************************")
             console.log(env_state)
@@ -168,9 +168,7 @@ const recheck_env_state = async (resolve) => {
                     env_state: env_state
                 })
             }
-        })} catch (err) {
-            console.log('Error: blehhhhhhhhhhhhh')
-        }
+        })
 
 }
 
