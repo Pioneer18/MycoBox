51d749e7b0acc217addf510f9c4a3e1af82f7175
Jonathan Sells <pioneer@jonathans-air.home>
2021-09-19 13:57:40 -0400

    debugging

--- MycoBox/services/system.service/system.service.js
+++ MycoBox/services/system.service/system.service.js
@@ -4,9 +4,9 @@
  * Description: provides extra functionality to the system controller
  */
 const { temp_pid_controller_config, update_temperature } = require("../../controllers/environment.manager/temperature.controller");
-const { humidity_pid_controller_config, update_humidity, send_command} = require("../../controllers/environment.manager/humidity.controller");
+const { humidity_pid_controller_config, update_humidity, send_command } = require("../../controllers/environment.manager/humidity.controller");
 const { get } = require("../../globals/globals")
-const { update_environment_state } = require("../../controllers/sensors.controller/sensors.controller");
+const { update_environment_state, read_environment_state } = require("../../controllers/sensors.controller/sensors.controller");
 
 
 /**
@@ -27,19 +27,19 @@ const environment_manager = () => {
             if (validation) {
                 console.log('Environment Manager Has Validated Session')
                 //update_environment_state()
-                    run_pid_controllers()
-                        .then(data => {
-                            // if there is any data returned do whatever with it here
-                            // otherwise recall environment_manager, because the session must still be active
-                            console.log('#############################################################################')
-                            console.log('Update Value Returned | ' + data + ' | Recalling ENV MANAGER')
-                            console.log('#############################################################################')
+                run_pid_controllers()
+                    .then(data => {
+                        // if there is any data returned do whatever with it here
+                        // otherwise recall environment_manager, because the session must still be active
+                        console.log('#############################################################################')
+                        console.log('Update Value Returned | ' + data + ' | Recalling ENV MANAGER')
+                        console.log('#############################################################################')
 
-                            setTimeout(() => {
-                                console.log('**************************** Waited 2 Seconds ****************************')
-                                return environment_manager();
-                            }, 3000);
-                        })
+                        setTimeout(() => {
+                            console.log('**************************** Waited 2 Seconds ****************************')
+                            return environment_manager();
+                        }, 3000);
+                    })
 
             }
             if (!validation) {
@@ -75,31 +75,30 @@ Promise.all([promise1, promise2, promise3]).then((values) => {
 const run_pid_controllers = () => {
     console.log('Running PID Controllers now ------------------------------------------------------------------------')
     return new Promise((resolve) => {
-        update_environment_state()
-            .then(()=> validate_env_state()
-                .then(validation => {
-                    if (validation.validation) {
-                        const measured = calculate_measured(validation.env_state);
-                        // =========================================================================================================
-                        // todo: check for session stage (sr, pi, fr) 
-                        // generate config for each controller: add the other controller functions for this
-                        get_state()
-                            .then(state => {
-                                // pass correct stage to the pid controllers to select the correct env_config, different setpoints for each stage!
-                                console.log(state)
-                                const temp_config = temp_pid_controller_config(measured, state[0].spawn_running, state[2].temperature)
-                                const humidity_config = humidity_pid_controller_config(measured, state[0].spawn_running, state[2].humidity)
-                                update_temperature(temp_config)
-                                update_humidity(humidity_config)
-                                // update_ventilation - co2 reading (temp and humidity are considered)
-                                // update_circulation configuration selected state, not a pid
-                                return
-                            })
-                            .then(results => resolve(results))
-                    }
-                }))
-        
-    })
+        validate_env_state()
+            .then(validation => {
+                if (validation.validation) {
+                    const measured = calculate_measured(validation.env_state);
+                    // =========================================================================================================
+                    // todo: check for session stage (sr, pi, fr) 
+                    // generate config for each controller: add the other controller functions for this
+                    get_state()
+                        .then(state => {
+                            // pass correct stage to the pid controllers to select the correct env_config, different setpoints for each stage!
+                            console.log(state)
+                            const temp_config = temp_pid_controller_config(measured, state[0].spawn_running, state[2].temperature)
+                            const humidity_config = humidity_pid_controller_config(measured, state[0].spawn_running, state[2].humidity)
+                            update_temperature(temp_config)
+                            update_humidity(humidity_config)
+                            // update_ventilation - co2 reading (temp and humidity are considered)
+                            // update_circulation configuration selected state, not a pid
+                            return
+                        })
+                        .then(results => resolve(results))
+                }
+            })
+
+})
 }
 
 /**
@@ -111,38 +110,41 @@ const validate_env_state = () => {
     console.log('METHOD CALL: validate_env_state ----------------------–----------------------–----------------------–')
     // get the latet environment state
     return new Promise((resolve) => {
-        get('environment_state')
-            .then(env_state => {
-                console.log("Environment State: *****************************************")
-                console.log(env_state);
-                if (typeof env_state.internal_temp_1 !== 'number' ||
-                    typeof env_state.internal_temp_2 !== 'number' ||
-                    typeof env_state.internal_temp_3 !== 'number' ||
-                    typeof env_state.precise_temp_c !== 'number' ||
-                    typeof env_state.internal_humidity_1 !== 'number' ||
-                    typeof env_state.internal_humidity_2 !== 'number' ||
-                    typeof env_state.internal_humidity_3 !== 'number'
-                ) {
-                    console.log('Validate Env Recall: Blank Env State')
-                    update_environment_state()
-                        .then(() => {
-                            setTimeout(() => {
-                                recheck_env_state(resolve)
-                            }, 8000);
-                        })
-                }
-                if (typeof env_state.internal_temp_1 === 'number' ||
-                    typeof env_state.internal_temp_2 === 'number' ||
-                    typeof env_state.internal_temp_3 === 'number' ||
-                    typeof env_state.precise_temp_c === 'number' ||
-                    typeof env_state.internal_humidity_1 === 'number' ||
-                    typeof env_state.internal_humidity_2 === 'number' ||
-                    typeof env_state.internal_humidity_3 === 'number') {
-                    return resolve({
-                        validation: true,
-                        env_state: env_state
+        update_environment_state()
+            .then(() => {
+                read_environment_state()
+                    .then(env_state => {
+                        console.log("Environment State: *****************************************")
+                        console.log(env_state);
+                        if (typeof env_state.internal_temp_1 !== 'number' ||
+                            typeof env_state.internal_temp_2 !== 'number' ||
+                            typeof env_state.internal_temp_3 !== 'number' ||
+                            typeof env_state.precise_temp_c !== 'number' ||
+                            typeof env_state.internal_humidity_1 !== 'number' ||
+                            typeof env_state.internal_humidity_2 !== 'number' ||
+                            typeof env_state.internal_humidity_3 !== 'number'
+                        ) {
+                            console.log('Validate Env Recall: Blank Env State')
+                            update_environment_state()
+                                .then(() => {
+                                    setTimeout(() => {
+                                        recheck_env_state(resolve)
+                                    }, 8000);
+                                })
+                        }
+                        if (typeof env_state.internal_temp_1 === 'number' ||
+                            typeof env_state.internal_temp_2 === 'number' ||
+                            typeof env_state.internal_temp_3 === 'number' ||
+                            typeof env_state.precise_temp_c === 'number' ||
+                            typeof env_state.internal_humidity_1 === 'number' ||
+                            typeof env_state.internal_humidity_2 === 'number' ||
+                            typeof env_state.internal_humidity_3 === 'number') {
+                            return resolve({
+                                validation: true,
+                                env_state: env_state
+                            })
+                        }
                     })
-                }
             })
 
     })
