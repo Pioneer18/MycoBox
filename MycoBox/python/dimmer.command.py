9d87e4313ad73201db8d0a4acd0b50ff4d3674cd
Jonathan Sells <pioneer@jonathans-air.home>
2021-09-21 08:58:47 -0400

    modified dimmer.command.py to match raspi.to.mega.py

--- MycoBox/python/dimmer.command.py
+++ MycoBox/python/dimmer.command.py
@@ -2,13 +2,26 @@
 # This file needs to be generic and usable by any PID controller
 # Parameters: Command
 # How it works: Send command and...
+# import serial
+# import time
+# import sys
+# ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+# ser.flush()
+# command = sys.argv[1] + '\n'
+# ser.write(command.encode())
+# time.sleep(3)
+# print("Command Sent!")
 import serial
 import time
 import sys
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 ser.flush()
+finished = False
 command = sys.argv[1] + '\n'
-ser.write(command.encode())
-time.sleep(3)
-print("Command Sent!")
-
+while finished == False:
+    ser.write(command.encode())
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8')
+        print(line)
+        finished = True
+    time.sleep(1)
