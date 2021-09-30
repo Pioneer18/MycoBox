e2431ebbaae4ac79470bfceaf197eff714d831dd
Jonathan Sells <pioneer@jonathans-air.home>
2021-09-29 22:42:17 -0400

    building out the step_tester_cli: the inquirer prompter for building a test configuration

--- MycoBox/cli_step_tester/index.js
+++ MycoBox/cli_step_tester/index.js
@@ -39,15 +39,127 @@
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 const log = console.log;
-const {} = require('../cli_control_panel/relay');
+const { } = require('../cli_control_panel/relay');
+
+// Intro Description
+log(chalk.black.bgYellow('Environment Manager Step Tester'))
+log(chalk.white("================================================================================================"))
+log(chalk.yellow("The Step Tester Wizard will prompt you to configure one or multiple tests for this session.\n"
+    + "Once you've created and submitted the tests you would like to run, the Step Tester will begin the session.\n"
+    + "Each session has a " + chalk.italic.blue("Session Report") + " directory with a file for each test it ran.\n"
+    + "Session Reports are saved in the " + chalk.italic.blue("SessionReports") + " directory in the MycoBox directory.\n"
+    + chalk.white("==============================================================================================\n")
+    + "Steps to Creating a Test:\n"
+    + "#1. Title\n"
+    + "#2. Starting Criteria: Which variables have a required starting range?\n"
+    + "#3. What's the range for each of the selected variables?\n"
+    + "#4. Which Actuators will be running?\n"
+    + "#5. What is the output value for each selected actuator: 1 - 410\n"
+    + "#6. How many read cycles for the test?\n"
+    + "#6. Would you like to add another test to the session?\n"
+))
+
+// Test Session Configuration
+let configuration = {};
 
 /**
- * Configure Test(s)
+ * First Questions
+ * 1. Title
+ * 2. Which Environment Variables are being tested
  */
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter a title for this test',
+        validate(value) {
+            if (typeof value === 'string' && value.length > 5) return true
+            return 'Please enter a longer title'
+        }
+    },
+    {
+        type: 'checkbox',
+        message: 'Select which environment variables are being tested',
+        name: 'env_variables',
+        choices: [
+            { name: 'Temperature' },
+            { name: 'Humidity' },
+            { name: 'CO2' }
+        ]
+    }
+];
+
+/**
+ * First Prompter
+ * 1. for each selected environment variable generate a prompt for the min and max starting value
+ * 2. map the answers to the configuration
+ */
+inquirer.prompt(questions)
+    .then((answers) => {
+        configuration.title = answers.title;
+        let questions = [];
+        log(chalk.white('\nDebug 1'));
+        log(chalk.white('\nanswers 1'));
+        log(chalk.white(JSON.stringify(answers, null, '  ')));
+        // Ask for the Maximum starting value of each selected variable
+        answers.env_variables.forEach(variable => {
+            if (variable === 'Temperature') {
+                questions.push({
+                    type: 'input',
+                    message: 'Please enter the maximum temperautre in Celsisus acceptable for starting the test',
+                    name: 'tempMinimum',
+                    validate(value) {
+                        if (value = '' || value === undefined || (parseInt(value) >= 0 && parseInt(value) <= 35)) return true
+                        return 'Please choose a value larger than 0 and less than 35 degrees C'
+                    }
+                })
+            }
+            if (variable === 'Humidity') {
+                questions.push({
+                    type: 'input',
+                    message: 'Please enter the maximum Relative Humidity acceptable for starting the test',
+                    name: 'rhMinimum',
+                    validate(value) {
+                        if (value = '' || value === undefined || (parseInt(value) >= 10 && parseInt(value) <= 100)) return true
+                        return 'Please choose a value larger than 0 and less than 35 degrees C'
+                    }
+                })
+            }
+            if (variable === 'CO2') {
+                questions.push({
+                    type: 'input',
+                    message: 'Please enter the maximum CO2 ppm acceptable for starting the test',
+                    name: 'co2Minimum',
+                    validate(value) {
+                        if (value = '' || value === undefined || (parseInt(value) >= 200 && parseInt(value) <= 20000)) return true
+                        return 'Please choose a value larger than 199 and less than 20001 degrees C'
+                    }
+                })
+            }
+        })
+        return questions
+    })
+    .then((questions) => {
+        /**
+         * Second Prompter
+         * 1. ask the follow up qualifying questions
+         * 2. map the answers to the configuration
+         */
+        inquirer.prompt(questions).then((answers) => {
+            configuration = {...configuration,...answers }
+            log(chalk.white('Debug 2'));
+            log(chalk.white("--------------------------------"))
+            log(chalk.white("answers"))
+            log(chalk.white(JSON.stringify(answers, null, '  ')))
+            log(chalk.white("configuration"))
+            log(chalk.yellow(JSON.stringify(configuration, null, '  ')))
+        })
+    })
+
 
 
 /**
- * Run Tester
+ * Run Tests
  * Steps:
  * - Set the overrides_config (no env_config because we're overriding and sending specific actuator outputs)
  * - Update the environment state: make sure the sensors are working
@@ -61,6 +173,9 @@ const {} = require('../cli_control_panel/relay');
  *      - report readings to a test log folder/ file per test
  *      
  */
+const run_tests = () => {
+
+}
 
 /**
  * Post Test
