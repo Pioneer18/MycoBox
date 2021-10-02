f009a08a8f63edf56e953c0e327de73b2876f8d2
Jonathan Sells <pioneer@jonathans-air.home>
2021-10-02 15:48:08 -0400

    removed default test config from front-end

--- FrontEnd/public/javascript/resources/default_configs.js
+++ FrontEnd/public/javascript/resources/default_configs.js
@@ -98,40 +98,7 @@ const default_configs = {
             flushess: '', // number of flush and domarncy cycles
             dormancy: '' // duration in between flushes
         }
-    },
-    test_config: { 
-        spawn_running: {
-            temperature: '25',
-            humidity: '75',
-            co2: '15000',
-            circulation_top: 'off',
-            circulation_bottom: 'off',
-            lighting: 'off',
-            trigger: true,
-            duration: null
-        },
-        primordia_init: {
-            user_confirmed: false,
-            duration: 3,
-            temperature: '25',
-            humidity: '85',
-            co2: '400',
-            circulation_top: 'Mid',
-            circulation_bottom: 'Mid',
-            lighting: 'On' // need better options for lighting
-        },
-        fruiting: {
-            temperature: '20',
-            humidity: '90',
-            co2: '400',
-            circulation_top: 'mid',
-            circulation_bottom: 'mid',
-            lighting: 'on',
-            duration: '14',
-            flushes: '3', // number of flush and domarncy cycles
-            dormancy: '10' // duration in between flushes
-        }
-    },
+    }
 }
 
 export {default_configs};
