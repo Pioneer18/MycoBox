55590a400974aa4dc441c06da9e58d2fcfcac8f2
Jonathan Sells <pioneer@jonathans-air.home>
2021-10-02 17:36:47 -0400

    testing why arduino not connecting for step_tester

--- MycoBox/cli_step_tester/read_environment_test.js
+++ MycoBox/cli_step_tester/read_environment_test.js
@@ -1,3 +1,31 @@
-const { update_environment_state } = require("../controllers/sensors.controller/sensors.controller");
+const { PythonShell } = require('python-shell');
 
-update_environment_state()
+/**
+ * DHT22 Temperature & humidity readings - internal & external
+ * @param {Array} reply [h1,h2,h3,t1,t2,t3]
+ */
+ const mega_temp_humidity = () => {
+    let mega = {
+        mode: 'text',
+        pythonOptions: ['-u'], // get print results in real-time
+        scriptPath: 'MycoBox/python',
+        args: ["D 5"] // Read All Sensors
+    };
+    console.log("Mega: Args")
+    return new Promise((resolve, reject) => {
+        console.log(__dirname)
+        PythonShell.run('raspi.to.mega.py', mega, function (err, reply) {
+            if (err) reject(err)
+            console.log('Should be reading mega data...')
+            if (!reply) {
+                // setTimeout(() => {
+                //     mega_temp_humidity()
+                // }, 13000);
+            }
+            read_mega_data(reply)
+                .then(resolve())
+        })
+    })
+}
+
+mega_temp_humidity()
