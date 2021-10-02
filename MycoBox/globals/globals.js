a44ba3881d51e2014fe7b7b4b7916d801ecaf63f
Jonathan Sells <pioneer@jonathans-air.home>
2021-10-02 12:07:31 -0400

    removing logs

--- MycoBox/globals/globals.js
+++ MycoBox/globals/globals.js
@@ -372,8 +372,6 @@ const set_overrides_state = (element, value) => {
 }
 
 const set_overrides_state_validation = (element, value) => {
-    console.log("Set Overrides State");
-    console.log(typeof value)
     try {
         if (element && element !== undefined && element !== ''
             && value && value !== undefined && value !== '') {
