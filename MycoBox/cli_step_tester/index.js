e1683bb4ef4ded86d5835dda18d4d4c719f973b7
Jonathan Sells <pioneer@jonathans-air.home>
2021-10-03 12:47:17 -0400

    hmmm

--- MycoBox/cli_step_tester/index.js
+++ MycoBox/cli_step_tester/index.js
@@ -303,12 +303,12 @@ const newTestSession = (config) => {
                         // run test_preparation: // wait for env to reset / push the env to where it needs to be before next test
                         set_session_state('cycles_limit', parseInt(test_config.cycles))
                             // call environment manager: in test mode env counts it's loops and ends session on final loop
-                            .then(() => environment_manager('TEST')
+                            // .then(() => environment_manager('TEST')
                                 .then(data => {
                                     console.log("Made it out of the EM!!!!!!!!!!!")
                                     console.log(data)
                                 })
-                                .catch(err => console.log(`Error Caught: New Test Session: ${err}`)))
+                                .catch(err => console.log(`Error Caught: New Test Session: ${err}`))//)
                     }))
         }
     })
