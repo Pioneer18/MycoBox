79ea4add043323e43b784486f91c7ba01cb43109
Jonathan Sells <pioneer@jonathans-air.home>
2021-10-04 16:21:02 -0400

    removed unnecessary log

--- MycoBox/logs/logger.js
+++ MycoBox/logs/logger.js
@@ -16,7 +16,7 @@ const test_file_path = () => {
             log(chalk.green('Logger`s working directory'))
             log(chalk.green(__dirname));
             log(chalk.magenta("-------------------------------------------"))
-            log(chalk.magenta(config))
+            log(chalk.magenta(JSON.stringify(config, null, '  ')))
             // create the file path: ../../
         })
 }
