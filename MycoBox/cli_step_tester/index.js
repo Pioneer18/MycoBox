43bccfbc870ee4433ed3cae622ee1200c15bfb46
Jonathan Sells <pioneer@jonathans-air.home>
2021-10-03 22:41:08 -0400

    store logs outside of RootCulture

--- MycoBox/cli_step_tester/index.js
+++ MycoBox/cli_step_tester/index.js
@@ -275,7 +275,7 @@ const run_tests = () => {
     // create directory for test log files
     if (!dirCreated) {
         log(chalk.redBright(__dirname));
-        const dir = `../logs/${timestamp()}`;
+        const dir = `../../../EM_LOGS/${timestamp()}`;
         log(chalk.blueBright(dir))
         if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir)
