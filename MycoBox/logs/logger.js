1cbe1cdb3ece4d1a9b5ea62d252beca74cd530e1
Jonathan Sells <pioneer@jonathans-air.home>
2021-10-05 22:37:36 -0400

    added some notes to the logger

--- MycoBox/logs/logger.js
+++ MycoBox/logs/logger.js
@@ -3,12 +3,31 @@ const chalk = require('chalk');
 const log = console.log;
 
 /**
+ * note: use seconds as the unit for the step tests/fopdt/PID controllers
+ * note: (DLO), defined as the expected values for set point and major disturbances during normal operation
+ * note: When the FOPDT dynamic model is fit to process data, the results describe how PV will respond to a change in CO via the model parameters. In particular:
  * Test Logger
+ * Goal:
+ * - Create an FOPDT model from the step test data; Kp, Tp, Өp
+ * - FOPDT: are to describe the overall dynamic behavior of the process with an approximating model
+ * - Why: differential equations relate 1 or more functions and their derivatives; quantities and their rates of chagne
+ *      - the direction the PV moves given a change in CO
+ *      - how far the PV ultimately travels for a given change in CO
+ *      - how fast the PV moves as it heads toward its new steady state
+ *      - how much delay occurs between when the CO changes and teh PV first begins to respond
  */
 const test_logger = () => {
-    // get the variables requested to be tracked for the test from the test config; temp, humidity, co2
-    // get the start criteria value(s)
-    // get the override values (if any)    
+    // On first log: when file created
+    // #1. get the start criteria value(s): where did the environment start from
+    // #2. get the step override values (COs) & the initial; some tests begin with actuators running!
+    // #3. get the total cycles (& total runtime)
+
+    // ---------------------------------------------------------------------------------------------------------
+    // #1. get the variables requested to be tracked for the test from the test config; temp, humidity, co2
+    // #2. grab the 'tracked variables' from the env_state that was used for the latest em cycle
+    // #3. grab the current total time and the dt
+    // $4. print the tracked PV(s), the dt, and the current total time & cycle count
+    
 
     /**
      *  Raw Data                              
