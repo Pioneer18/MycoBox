3d1aee00a0b2162f0abd667ca6a2190bbe392ea7
Jonathan Sells <pioneer@jonathans-air.home>
2021-11-13 13:59:09 -0500

    push to the real pvArray

--- MycoBox/logs/logger.js
+++ MycoBox/logs/logger.js
@@ -42,11 +42,10 @@ const test_logger = () => {
             let arr = [];
             const internal_temp = (((parseFloat(state[2].internal_temp_1)) + (parseFloat(state[2].internal_temp_2)) + (parseFloat(state[2].internal_temp_3)) + (parseFloat(state[2].precise_temp_c))) / 4).toFixed(2);
             const internal_humidity = (((parseFloat(state[2].internal_humidity_1)) + (parseFloat(state[2].internal_humidity_2)) + (parseFloat(state[2].internal_humidity_3))) / 3).toFixed(2);
-            const pvArray = [];
             const pv = currentPv(state[1], internal_humidity, internal_temp); // the value
             const et = elapsedTime(state[1].tests[0].process_var, state[3], state[0])
-            pvArray.push([state[1].tests[0].process_var, et]);
-            log(chalk.blueBright(pvArray));
+            state[3].pvArray.push([state[1].tests[0].process_var, et]);
+            log(chalk.blueBright(state[3].pvArray));
             // const internal_co2 = ...
 
             log(chalk.redBright(`Cycles Count: ${state[1].cycles_count}`))
